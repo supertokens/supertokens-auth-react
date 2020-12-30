c31f418dae4451c725f9f6686188526724664cb8
NkxxkN <kevin.antoine@protonmail.com>
2020-12-30 16:23:44 +0100

    fix npm link issues

--- test/linkDependencies.sh
+++ test/linkDependencies.sh
@@ -11,7 +11,7 @@ for dependency in "${dependencies[@]}"
 do
     (
         cd $1/node_modules/$dependency
-        npm link
+        npm link --only=production
     )
     npm link $dependency
 done
