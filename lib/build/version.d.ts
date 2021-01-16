3ed4c71bcfac4d6650e6a188c3a16ea7c2fce1c2
NkxxkN <kevin@supertokens.io>
2021-01-16 13:06:20 +0100

    fix mobile responsiveness (#131)
    * fix mobile responsiveness
    
    * bump to 0.4.1

--- lib/build/version.d.ts
+++ lib/build/version.d.ts
@@ -1,2 +1,2 @@
-export declare const package_version = "0.4.0";
+export declare const package_version = "0.4.1";
 export declare const supported_fdi: string[];
--- lib/ts/recipe/emailpassword/components/themes/default/styles/styles.ts
+++ lib/ts/recipe/emailpassword/components/themes/default/styles/styles.ts
@@ -87,21 +87,16 @@ export function getDefaultStyles(palette: NormalisedPalette): NormalisedDefaultS
     return {
         container: {
             fontFamily: "'Rubik', sans-serif",
-            margin: "26px auto",
-            width: "420px",
+            margin: "12px auto",
+            marginTop: "26px",
+            marginBottom: "26px",
+            maxWidth: "420px",
             textAlign: "center",
             borderRadius: "8px",
             boxShadow: "1px 1px 10px rgba(0,0,0,0.16)",
             backgroundColor: palette.colors.background,
             "@media (max-width: 440px)": {
-                margin: "12px auto",
-                minWidth: "320px"
-            },
-            "@media (max-width: 340px)": {
-                minWidth: "260px"
-            },
-            "@media (max-width: 300px)": {
-                minWidth: "220px"
+                maxWidth: "95vw"
             }
         },
 
@@ -281,7 +276,7 @@ export function getDefaultStyles(palette: NormalisedPalette): NormalisedDefaultS
             display: "flex",
             flexDirection: "column",
             paddingTop: "0px",
-            paddingBottom: "35px"
+            paddingBottom: "34px"
         },
 
         primaryText: {
