f672fff9c08749dfbe7d02db9796ea6d850d9667
Infi-Knight <soniravi829@gmail.com>
2021-02-01 17:41:45 +0530

    bootstrap frontend

---
+++ examples/with-one-login-many-subdomains/frontend/src/App.js
@@ -0,0 +1,15 @@
+import SuperTokens from "supertokens-auth-react";
+import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
+import Session from "supertokens-auth-react/recipe/session";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Multi Tenancy with SuperTokens
+    </div>
+  );
+}
+
+export default App;
