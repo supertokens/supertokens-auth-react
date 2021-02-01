98fd2ab50868137ffe3a26729ff47c12a66e6d56
Joel Coutinho <joelcoutinho@qually.io>
2021-02-01 15:49:19 +0530

    basic setup

--- examples/with-one-login-per-subdomain/src/App.js
+++ examples/with-one-login-per-subdomain/src/App.js
@@ -1,12 +1,54 @@
 import './App.css';
+import SuperTokens, { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react"
+import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
+import Session from "supertokens-auth-react/recipe/session";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+
+export function getApiDomain() {
+  const apiPort = process.env.REACT_APP_API_PORT || 3001;
+  const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+  return apiUrl;
+}
+
+export function getWebsiteDomain() {
+  const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+  const websiteUrl = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+  return websiteUrl;
+}
+
+SuperTokens.init({
+  appInfo: {
+    appName: "SuperTokens Demo App",
+    apiDomain: getApiDomain(),
+    websiteDomain: getWebsiteDomain()
+  },
+  recipeList: [
+    EmailPassword.init({
+      emailVerificationFeature: {
+        mode: "REQUIRED"
+      }
+    }),
+    Session.init()
+  ]
+});
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Test
-      </header>
-    </div>
+    <Router>
+      <div className="fill">
+        <Switch>
+          {getSuperTokensRoutesForReactRouterDom()}
+          <Route path="/">
+            <EmailPassword.EmailPasswordAuth>
+              <div>HomePage</div>
+            </EmailPassword.EmailPasswordAuth>
+          </Route>
+        </Switch>
+      </div>
+    </Router >
+  </div>
   );
 }
 
