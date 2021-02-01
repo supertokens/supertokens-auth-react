38708fad0fe704e979147d026d878d7f41a06910
Infi-Knight <soniravi829@gmail.com>
2021-02-01 19:56:32 +0530

    add server

--- examples/with-one-login-many-subdomains/frontend/src/App.js
+++ examples/with-one-login-many-subdomains/frontend/src/App.js
@@ -1,13 +1,56 @@
-import SuperTokens from "supertokens-auth-react";
+import './App.css';
+import SuperTokens, { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react"
 import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
 import Session from "supertokens-auth-react/recipe/session";
+import Home from "./Home";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from "./Footer";
 
-import './App.css';
+export function getApiDomain() {
+  const apiPort = process.env.REACT_APP_API_PORT || 3001;
+  const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+  return apiUrl;
+}
+
+export function getWebsiteDomain() {
+  const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+  const websiteUrl = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+  return websiteUrl;
+}
+
+SuperTokens.init({
+  appInfo: {
+    appName: "SuperTokens Demo App",
+    apiDomain: getApiDomain(),
+    websiteDomain: getWebsiteDomain()
+  },
+  recipeList: [
+    EmailPassword.init({
+      emailVerificationFeature: {
+        mode: "REQUIRED"
+      }
+    }),
+    Session.init()
+  ]
+});
+
 
 function App() {
   return (
     <div className="App">
-      Multi Tenancy with SuperTokens
+      <Router>
+        <div className="fill">
+          <Switch>
+            {getSuperTokensRoutesForReactRouterDom()}
+            <Route path="/">
+              <EmailPassword.EmailPasswordAuth>
+                <Home />
+              </EmailPassword.EmailPasswordAuth>
+            </Route>
+          </Switch>
+        </div>
+        <Footer />
+      </Router >
     </div>
   );
 }
