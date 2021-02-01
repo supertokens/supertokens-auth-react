f85ab94ee4d36c6b25632f842b7cbfe84630df70
Joel Coutinho <joelcoutinho@qually.io>
2021-02-01 15:08:16 +0530

    init

---
+++ examples/with-one-login-per-subdomain/api-server.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+let supertokens = require("supertokens-node");
+let Session = require("supertokens-node/recipe/session");
+let EmailPassword = require("supertokens-node/recipe/emailpassword");
+
+const apiPort = process.env.REACT_APP_API_PORT || 3001;
+const apiDomain = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+const websiteDomain = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`
+
+supertokens.init({
+    supertokens: {
+        connectionURI: "https://try.supertokens.io",
+    },
+    appInfo: {
+        appName: "SuperTokens Demo App",
+        apiDomain,
+        websiteDomain
+    },
+    recipeList: [
+        EmailPassword.init(),
+        Session.init()
+    ]
+});
+
+const app = express();
+
+
+app.use(cors({
+    origin: websiteDomain,
+    allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    credentials: true,
+}));
+
+app.use(supertokens.middleware());
+
+// custom API that requires session verification
+app.get("/sessioninfo", Session.verifySession(), async (req, res) => {
+    let session = req.session;
+    res.send({
+        sessionHandle: session.getHandle(),
+        userId: session.getUserId(),
+        jwtPayload: session.getJWTPayload(),
+        sessionData: await session.getSessionData(),
+    });
+});
+
+
+app.use(supertokens.errorHandler());
+
+app.use((err, req, res, next) => {
+    res.status(500).send("Internal error: " + err.message);
+})
+
+app.listen(apiPort, () => console.log(`API Server listening on port ${apiPort}`));
