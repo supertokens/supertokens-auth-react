deab7c8d3bc49bd040bf306b264b3a8a0999665b
Ravi Soni <soniravi829@gmail.com>
2021-02-05 17:28:51 +0530

    With one login many subdomains (#157)
    * bootstrap frontend
    
    * add server
    
    * add tenant username field
    
    * username validation
    
    * username validation on backend
    
    * redirect to a hardcoded subdomain
    
    * fix redirect
    
    * update auth react
    
    * extract subdomain from email, update README
    
    * get subdomain from email
    
    * test for reset-password
    
    * rm package-lock
    
    * rm package-lock
    
    * add postinstall script
    
    * fix typo
    
    * add regex whitelisting
    
    * copy paste joel's regex
    
    * update auth-react & password reset mail
    
    * remove unnecassary form field
    
    * fix signup, verify-email and reset redirection
    
    * update auth-react
    
    * return isUserEmailVerified along with subdomain
    
    * add proper redirection
    
    * remove redundant isUserVerified and spinner
    
    * cleanup
    
    * remove redundant callbacks
    
    * add comments and cleanup App.js
    
    Co-authored-by: NkxxkN <kevin.antoine@protonmail.com>

---
+++ examples/with-one-login-many-subdomains/frontend/src/utils.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import Session from "supertokens-auth-react/recipe/session";
+
+const apiPort = process.env.REACT_APP_API_PORT || 3001;
+export const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+export function getApiDomain() {
+  const apiUrl =
+    process.env.REACT_APP_API_URL || `http://example.com:${apiPort}`;
+  return apiUrl;
+}
+
+export const userSubdomainApi = `${getApiDomain()}/user-subdomain`;
+
+export function getAuthDomain() {
+  const websiteUrl =
+    process.env.REACT_APP_WEBSITE_URL ||
+    `http://auth.example.com:${websitePort}`;
+  return websiteUrl;
+}
+
+export async function getSubdomainForCurrentUser() {
+  const res = await axios.get(userSubdomainApi);
+  return res.data.subdomain;
+}
+
+export async function getRedirectionUrlForUser() {
+  const subdomain = await getSubdomainForCurrentUser();
+  return `http://${subdomain}.example.com:${websitePort}`;
+}
+
+export async function redirectIfOnWrongSubdomain() {
+  try {
+    if (Session.doesSessionExist()) {
+      const currentSubdomain = window.location.hostname.split(".")[0];
+      const currentUserSubdomain = await getSubdomainForCurrentUser();
+      // location.origin check ensures that user gets the option to click
+      // the continue button on verify-email page
+      if (
+        window.location.origin !== getAuthDomain() &&
+        currentSubdomain !== currentUserSubdomain
+      ) {
+        window.location.href = `http://${currentUserSubdomain}.example.com:${websitePort}`;
+      }
+    }
+  } catch (error) {}
+}
