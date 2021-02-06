d5548f537b8e6ec22c812e67b699aa2d144716c2
Joel Coutinho <joelsteve97@hotmail.com>
2021-02-06 12:13:24 +0530

    With one login per subdomain (#158)
    * init
    
    * init
    
    * basic setup
    
    * added .eslintcache to gitignore
    
    * remove .eslintcache
    
    * added header and footer components
    
    * fix cors issue for subdomain
    
    * testing implementation
    
    * configuring home screen
    
    * fixes for subdomains
    
    * updated supertokens-auth-react version
    
    * fixs for verification and password reset links
    
    * fixs to css, removed verification link logging
    
    * Update README.md
    
    * Update README.md
    
    * fix typos in README.md
    
    * Update examples/with-one-login-per-subdomain/README.md
    
    Co-authored-by: NkxxkN <kevin@supertokens.io>
    
    * update docs, regex for whitelisting domains on example.com
    
    * removed package-lock.json
    
    * updated whitelist regex
    
    * removed regex for getting subdomain
    
    * changes to readme
    
    * changes according to Rishabhs feedback
    
    * removed unnecessary function
    
    Co-authored-by: Joel Coutinho <joelcoutinho@qually.io>
    Co-authored-by: NkxxkN <kevin@supertokens.io>
    Co-authored-by: Rishabh Poddar <rishabh.poddar@gmail.com>

---
+++ examples/with-one-login-per-subdomain/src/Home/SuccessView.js
@@ -0,0 +1,45 @@
+import CallAPIView from "./CallAPIView";
+import {getApiDomain} from "../utils"
+
+export default function SuccessView(props) {
+    let userId = props.userId;
+    let domain = getApiDomain()
+
+    return (
+        <div
+            className="fill"
+            style={{
+                width: "100%",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+                fontWeight: "bold",
+                color: "#333333",
+                paddingTop: "10px",
+                paddingBottom: "40px"
+            }}>
+
+            <span
+                style={{
+                    fontSize: "50px"
+                }}>🥳🎉</span>
+            Login successful at <div />{domain}
+            <div style={{ height: "10px" }} />
+            <div style={{ height: "10px" }} />
+            Your user ID is<div />{userId}
+            <div style={{ height: "10px" }} />
+            <div style={{ height: "10px" }} />
+            <div style={{ height: "10px" }} />
+            <div style={{ height: "10px" }} />
+            <CallAPIView />
+            <div style={{ height: "10px" }} />
+            <div style={{ height: "10px" }} />
+            <div style={{ height: "10px" }} />
+                ------------------------------------
+            <div style={{ height: "10px" }} />
+            <div style={{ height: "10px" }} />
+            <div style={{ height: "10px" }} />
+            <div style={{ height: "10px" }} />
+        </div>
+    );
+}
