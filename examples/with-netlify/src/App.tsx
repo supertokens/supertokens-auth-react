428dd35f1e915f60b91b2156f74639d10715d0fb
Rishabh Poddar <rishabh.poddar@gmail.com>
2021-03-04 17:32:52 +0530

    gets frontend to work on netlify

---
+++ examples/with-netlify/src/App.tsx
@@ -0,0 +1,52 @@
+import './App.css';
+import SuperTokens, { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react"
+import EmailPassword, { EmailPasswordAuth } from "supertokens-auth-react/recipe/emailpassword";
+import Session from "supertokens-auth-react/recipe/session";
+import Home from "./Home";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from "./Footer";
+
+export function getApiDomain() {
+  const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:8082`;
+  return apiUrl;
+}
+
+export function getWebsiteDomain() {
+  const websiteUrl = process.env.REACT_APP_WEBSITE_URL || `http://localhost:3002`;
+  return websiteUrl;
+}
+
+
+SuperTokens.init({
+  appInfo: {
+    appName: "SuperTokens Demo App",
+    apiDomain: window.location.hostname,
+    websiteDomain: window.location.hostname
+  },
+  recipeList: [EmailPassword.init(), Session.init()]
+});
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div className="fill">
+          <Switch>
+            {getSuperTokensRoutesForReactRouterDom()}
+            <Route path="/">
+              <EmailPasswordAuth>
+                <Home />
+              </EmailPasswordAuth>
+            </Route>
+          </Switch>
+        </div>
+        <div className="footer">
+          <Footer />
+        </div>
+      </div>
+    </Router >
+  );
+}
+
+export default App;
---
+++ examples/with-netlify/src/Footer/index.tsx
@@ -0,0 +1,19 @@
+export default function Footer(): JSX.Element {
+    return (
+        <div
+            style={{
+                display: "flex",
+                width: "100%",
+                height: "80px",
+                backgroundColor: "#000000",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+                alignSelf: "flex-end",
+                color: "#ffffff",
+                fontWeight: "bold"
+            }}>
+            React Demo app. Made with ❤️ using SuperTokens.io
+        </div>
+    );
+}
