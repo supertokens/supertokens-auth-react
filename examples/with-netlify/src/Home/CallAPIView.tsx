0dd7e04223b10a99f16b5ebcc8f83cf443700313
Bhumil Sarvaiya <sarvaiyabhumil@gmail.com>
2021-04-27 16:40:03 +0530

    code review changes

--- examples/with-netlify/src/Home/CallAPIView.tsx
+++ examples/with-netlify/src/Home/CallAPIView.tsx
@@ -1,28 +1,32 @@
 import axios from "axios";
 import Session from "supertokens-auth-react/recipe/session";
+import Emailpassword from "supertokens-auth-react/recipe/emailpassword";
 import { useHistory } from "react-router-dom";
 import { getDomain } from "../App";
 Session.addAxiosInterceptors(axios);
 
 export default function CallAPIView() {
-    const history = useHistory();
+  const history = useHistory();
 
-    async function callAPIClicked() {
-        // this will also automatically refresh the session if needed
-        try {
-            let response = await axios.get("https://" + getDomain() + "/.netlify/functions/user");
-            window.alert("Session Information:\n" + JSON.stringify(response.data, null, 2));
-        } catch (err) {
-            if (err.status === 401) {
-                window.alert("Oops! Your session has expired!");
-                history.push("/auth");
-            }
-        }
+  async function callAPIClicked() {
+    // this will also automatically refresh the session if needed
+    try {
+      let response = await axios.get(
+        "https://" + getDomain() + "/.netlify/functions/user"
+      );
+      window.alert(
+        "Session Information:\n" + JSON.stringify(response.data, null, 2)
+      );
+    } catch (err) {
+      if (err.status === 401) {
+        Emailpassword.redirectToAuth();
+      }
     }
+  }
 
-    return (
-        <div onClick={callAPIClicked} className="sessionButton">
-            Call API
-        </div>
-    );
+  return (
+    <div onClick={callAPIClicked} className="sessionButton">
+      Call API
+    </div>
+  );
 }
