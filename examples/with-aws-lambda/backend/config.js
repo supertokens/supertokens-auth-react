40d721f483068bad5dda7e8fafdbfca159063272
Bhumil Sarvaiya <sarvaiyabhumil@gmail.com>
2021-04-28 20:06:59 +0530

    aws example updated to latest version of node and auth-react package (#236)

--- examples/with-aws-lambda/backend/config.js
+++ examples/with-aws-lambda/backend/config.js
@@ -11,9 +11,9 @@ module.exports.getBackendConfig = () => {
       apiDomain: "https://0ktsu4mmb6.execute-api.us-east-1.amazonaws.com",
       websiteDomain: "http://localhost:8888",
       apiBasePath: "/auth",
+      apiGatewayPath: "/dev", // use this if you are using AWS API Gateway. The value should be equal to the stage value. i.e. if your stage in API Gateway is prod, this value should be /prod
     },
     recipeList: [EmailPassword.init(), Session.init()],
     isInServerlessEnv: true,
-    apiWebProxyPath: "/dev", // use this if you are using AWS API Gateway. The value should be equal to the stage value. i.e. if your stage in API Gateway is prod, this value should be /prod
   };
 };
