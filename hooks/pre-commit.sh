a467c61c9ebae1220a86d345d2c75aeaaf0943cc
Rishabh Poddar <rishabh.poddar@gmail.com>
2021-05-05 23:45:55 +0530

    should commit

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -99,7 +99,7 @@ then
    git stash drop >/dev/null 2>/dev/null
 fi
 
-if [ $compiles -eq 0 ] && [ $formatted -eq 0 ] && [ $linted -eq 0 ] && [ $pruned -eq 0 ]
+if [ $compiles -eq 0 ] && [ $formatted -eq 0 ] && [ $linted -eq 0 ] && [ $pruned -eq 0 ] && [ $dotOnly -eq 1]
 then
    echo "$(tput setaf 2)... done. Proceeding with commit.$(tput sgr 0)"
    echo ""
@@ -125,6 +125,12 @@ elif [ $compiles -ne 0 ]
 then
    echo "$(tput setaf 1)... done.$(tput sgr 0)"
    echo "$(tput setaf 1)CANCELLING commit due to COMPILE ERROR.$(tput sgr 0)"
+   echo ""
+   exit 2
+elif [ $dotOnly -ne 1]
+then
+   echo "$(tput setaf 1)... done.$(tput sgr 0)"
+   echo "$(tput setaf 1)CANCELLING commit due to .only in test files $(tput sgr 0)"
    echo ""
    exit 2
 fi
