9379e18eb2a3d3b1770a66c0260bcc702b333ee6
Marek Dulowski <dulowski.marek@gmail.com>
2021-06-28 13:11:35 +0200

    Add isDefaultContext SessionContext test

---
+++ test/unit/recipe/session/SessionContext.test.ts
@@ -0,0 +1,34 @@
+import { isDefaultContext } from "../../../../lib/ts/recipe/session/SessionContext";
+import { SessionContextType } from "../../../../lib/ts/recipe/session";
+
+describe("SessionContext", () => {
+    test("return true if user id is DEFAULT_USER_ID", () => {
+        // given
+        const mockSessionContext: SessionContextType = {
+            jwtPayload: {},
+            userId: "DEFAULT_USER_ID",
+            doesSessionExist: false,
+        };
+
+        // when
+        const isDefault = isDefaultContext(mockSessionContext);
+
+        // then
+        expect(isDefault).toBe(true);
+    });
+
+    test("return false if user id isn't DEFAULT_USER_ID", () => {
+        // given
+        const mockSessionContext: SessionContextType = {
+            jwtPayload: {},
+            doesSessionExist: false,
+            userId: "NOT_DEFAULT_USER_ID",
+        };
+
+        // when
+        const isDefault = isDefaultContext(mockSessionContext);
+
+        // then
+        expect(isDefault).toBe(false);
+    });
+});
