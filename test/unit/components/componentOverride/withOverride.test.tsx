409e92ef2fe88b2f4765a153b8e60a6b9d019eca
Marek Dulowski <dulowski.marek@gmail.com>
2021-07-02 17:21:45 +0200

    Add test for overriden component state loss and React default behaviour

--- test/unit/components/componentOverride/withOverride.test.tsx
+++ test/unit/components/componentOverride/withOverride.test.tsx
@@ -1,26 +1,41 @@
 import React, { useState } from "react";
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { act, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { withOverride } from "../../../../lib/ts/components/componentOverride/withOverride";
 import { ComponentOverrideContext } from "../../../../lib/ts/components/componentOverride/componentOverrideContext";
 
-const OverridenComponent = ({ foo }: { foo: string }) => {
-    const [counter, setCounter] = useState(0);
+const OverridenComponentFactory =
+    (OriginalComponent: React.FC<{ foo: string }>) =>
+    ({ foo }: { foo: string }) => {
+        const [useOriginalComponent, setUseOriginalComponent] = useState(false);
 
-    return (
-        <>
-            <span>Counter: {counter}</span>
-            <span>Foo: {foo}</span>
-            <button onClick={() => setCounter((c) => c + 1)}>Update</button>
-        </>
-    );
-};
+        const SwitchButton = () => (
+            <button onClick={() => setUseOriginalComponent((useOriginalComponent) => !useOriginalComponent)}>
+                Switch
+            </button>
+        );
+
+        return (
+            <>
+                {useOriginalComponent ? <OriginalComponent foo={foo} /> : <h1 data-testid="override">Override</h1>}
+                <SwitchButton />
+            </>
+        );
+    };
 
 describe("withOverride", () => {
-    const DefaultComponent = withOverride("DefaultComponent", ({ foo }: { foo: string }) => (
-        <h1 data-testid="heading">Default</h1>
-    ));
+    const DefaultComponent = withOverride("DefaultComponent", ({ foo }: { foo: string }) => {
+        const [counter, setCounter] = useState(0);
+
+        return (
+            <>
+                <span>Counter: {counter}</span>
+                <span>Foo: {foo}</span>
+                <button onClick={() => setCounter((c) => c + 1)}>Increment</button>
+            </>
+        );
+    });
 
     test("throw an error if no parent provider is set", async () => {
         // when
@@ -33,7 +48,7 @@ describe("withOverride", () => {
     test("display overriden component if context key matches override key", async () => {
         // given
         const overrides = {
-            DefaultComponent: () => OverridenComponent,
+            DefaultComponent: OverridenComponentFactory,
         };
 
         // when
@@ -44,14 +59,13 @@ describe("withOverride", () => {
         );
 
         // then
-        expect(await result.findByText(/^Counter:/i)).toHaveTextContent(/Counter: 0/i);
-        expect(await result.findByText(/^Foo:/i)).toHaveTextContent(/Foo: bar/i);
+        expect(await result.findByTestId("override")).toHaveTextContent(/Override/i);
     });
 
     test("preserve component state across rerenders", async () => {
         // given
         const overrides = {
-            DefaultComponent: () => OverridenComponent,
+            DefaultComponent: OverridenComponentFactory,
         };
 
         // when
@@ -61,25 +75,94 @@ describe("withOverride", () => {
             </ComponentOverrideContext.Provider>
         );
 
-        const Counter = await result.findByText(/^Counter:/i);
-        const Button = await result.findByText(/Update/i);
-        const Foo = await result.findByText(/Foo/i);
+        const OverrideContent = () => result.findByTestId("override");
+        const SwitchButton = () => result.findByText(/Switch/i);
+        const Counter = () => result.findByText(/^Counter:/i);
+        const IncrementButton = () => result.findByText(/Increment/i);
 
-        expect(Counter).toHaveTextContent("Counter: 0");
-        expect(Foo).toHaveTextContent("bar");
+        let overrideContent = await OverrideContent();
 
-        userEvent.click(Button);
+        expect(overrideContent).toBeInTheDocument();
 
-        expect(Counter).toHaveTextContent("Counter: 1");
+        // display original component
+        userEvent.click(await SwitchButton());
 
-        result.rerender(
-            <ComponentOverrideContext.Provider value={overrides}>
-                <DefaultComponent foo="baz" />
-            </ComponentOverrideContext.Provider>
-        );
+        expect(overrideContent).not.toBeInTheDocument();
+        expect(await Counter()).toHaveTextContent("Counter: 0");
+
+        // set state on original component
+        userEvent.click(await IncrementButton());
+
+        expect(await Counter()).toHaveTextContent("Counter: 1");
+
+        // display overriden component
+        userEvent.click(await SwitchButton());
+
+        overrideContent = await OverrideContent();
+
+        expect(overrideContent).toBeInTheDocument();
+
+        // display original component
+        userEvent.click(await SwitchButton());
 
         // then
-        expect(Counter).toHaveTextContent("Counter: 1");
-        expect(Foo).toHaveTextContent("baz");
+        expect(await Counter()).toHaveTextContent("Counter: 1");
+    });
+
+    test("original react behaviour", async () => {
+        const First = () => {
+            const [counter, setCounter] = useState(0);
+
+            return (
+                <>
+                    <span>First</span>
+                    <span>Counter: {counter}</span>
+                    <button onClick={() => setCounter((c) => c + 1)}>Increment</button>
+                </>
+            );
+        };
+
+        const Second = () => {
+            return (
+                <>
+                    <span>Second</span>
+                </>
+            );
+        };
+
+        const SwitchingComponent = () => {
+            const [useFirst, setUseFirst] = useState(true);
+
+            return (
+                <>
+                    {useFirst ? <First /> : <Second />}
+                    <button onClick={() => setUseFirst((use) => !use)}>Switch</button>
+                </>
+            );
+        };
+
+        const result = render(<SwitchingComponent />);
+
+        const SwitchButton = () => result.findByText(/Switch/i);
+        const FirstHeading = () => result.findByText(/First/i);
+        const SecondHeading = () => result.findByText(/Second/i);
+        const IncrementFirst = () => result.findByText(/Increment/i);
+        const FirstCounter = () => result.findByText(/^Counter:/i);
+
+        expect(await FirstHeading()).toBeInTheDocument();
+        expect(await FirstCounter()).toHaveTextContent("Counter: 0");
+
+        userEvent.click(await IncrementFirst());
+
+        expect(await FirstCounter()).toHaveTextContent("Counter: 1");
+
+        userEvent.click(await SwitchButton());
+
+        expect(await SecondHeading()).toBeInTheDocument();
+
+        userEvent.click(await SwitchButton());
+
+        expect(await FirstHeading()).toBeInTheDocument();
+        expect(await FirstCounter()).toHaveTextContent("Counter: 1");
     });
 });
