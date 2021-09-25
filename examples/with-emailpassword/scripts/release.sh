8c49efb1db503bde1aa13711b047589f5a8f105f
Rishabh <rishabh@supertokens.io>
2021-09-25 16:11:14 +0530

    updates start container scripts for demo examples

--- examples/with-emailpassword/scripts/release.sh
+++
@@ -1,3 +0,0 @@
-tag=$1$(date '+%d-%m-%Y--%H-%M-%S')
-git tag $tag
-git push origin --tags
--- examples/with-thirdparty/scripts/start_container.sh
+++ examples/with-thirdparty/scripts/start_container.sh
@@ -2,7 +2,7 @@ docker run -d \
     --restart=always \
     --name demo-app-thirdparty \
     --label name=demo-app-thirdparty \
-    --volume /home/ubuntu/supertokens-demo-react-thirdparty:/usr/src/app \
+    --volume /home/ubuntu/supertokens-auth-react/examples/with-thirdparty:/usr/src/app \
     --publish 10002:10000 \
     --publish 10003:10001 \
     supertokens/demo
