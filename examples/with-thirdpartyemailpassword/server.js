0505fe94b0acb48e96e0b1c43f3ef6229393fc7d
Rishabh <rishabh@supertokens.io>
2021-09-25 17:00:37 +0530

    updates demo apps

--- examples/with-thirdpartyemailpassword/server.js
+++ examples/with-thirdpartyemailpassword/server.js
@@ -5,10 +5,7 @@ const { join } = require("path");
 
 const app = express();
 
-const apiPort = process.env.REACT_APP_API_PORT || 3001;
-const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
 const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
-const websiteUrl = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
 
 app.use(morgan("dev"));
 app.use(
