bf3d2cd9940eda09a8a3c69e39ebf005840b2e05
jscyo <joelsteve97@hotmail.com>
2021-10-13 15:08:52 +0530

    fixs comment

--- examples/with-account-linking/api/index.js
+++ examples/with-account-linking/api/index.js
@@ -28,9 +28,9 @@ supertokens.init({
     recipeList: [
         ThirdPartyEmailPassword.init({
             providers: [
+                // We have provided you with development keys which you can use for testing.
+                // IMPORTANT: Please replace them with your own OAuth keys for production use.
                 ThirdPartyEmailPassword.Google({
-                    // We have provided you with development keys which you can use for testing.
-                    // IMPORTANT: Please replace them with your own OAuth keys for production use.
                     clientId: "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
                     clientSecret: "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",
                 }),
