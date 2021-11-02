6b4230c79ba8e4bbc053c296fdbc9c4d52ffcac2
Rishabh <rishabh@supertokens.io>
2021-11-02 14:04:58 +0530

    adds apple login to with-thirdparty example as well

--- examples/with-thirdparty/src/App.js
+++ examples/with-thirdparty/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import SuperTokens, { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react";
-import ThirdParty, { ThirdPartyAuth, Google, Github } from "supertokens-auth-react/recipe/thirdparty";
+import ThirdParty, { ThirdPartyAuth, Google, Github, Apple } from "supertokens-auth-react/recipe/thirdparty";
 import Session from "supertokens-auth-react/recipe/session";
 import Home from "./Home";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
@@ -29,7 +29,7 @@ SuperTokens.init({
     recipeList: [
         ThirdParty.init({
             signInAndUpFeature: {
-                providers: [Github.init(), Google.init()],
+                providers: [Github.init(), Google.init(), Apple.init()],
             },
             emailVerificationFeature: {
                 mode: "REQUIRED",
