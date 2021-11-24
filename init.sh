05aaadc813782e55db7501f3ac752746b425b732
Rishabh <rishabh@supertokens.io>
2021-11-25 01:02:38 +0530

    adds a comment to init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 # This script is used to initialise the dev env for this package.
 # Depending on the node version being used, it will do slightly different actions 
 
+# For Apple M1 chip, you will also have to add these to to your bash profile:
+# export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
+# export PUPPETEER_EXECUTABLE_PATH=`which chromium`
+# And run brew install chromium --no-quarantine
+
 version=`node --version`
 
 while IFS='.' read -ra ADDR; do
