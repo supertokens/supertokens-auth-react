690df97781732611a3f1e9af7405619030b7bd9b
Rishabh <rishabh@supertokens.io>
2021-12-22 16:56:06 +0530

    adds passwordless with phone demo app

---
+++ examples/with-passwordless-phone/src/App.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import "./App.css";
+import SuperTokens, { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react";
+import Passwordless from "supertokens-auth-react/recipe/passwordless";
+import Session from "supertokens-auth-react/recipe/session";
+import Home from "./Home";
+import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from "./Footer";
+import SessionExpiredPopup from "./SessionExpiredPopup";
+
+export function getApiDomain() {
+    const apiPort = process.env.REACT_APP_API_PORT || 3001;
+    const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+    return apiUrl;
+}
+
+export function getWebsiteDomain() {
+    const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+    const websiteUrl = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+    return websiteUrl;
+}
+
+SuperTokens.init({
+    appInfo: {
+        appName: "SuperTokens Demo App", // TODO: Your app name
+        apiDomain: getApiDomain(), // TODO: Change to your app's API domain
+        websiteDomain: getWebsiteDomain(), // TODO: Change to your app's website domain
+    },
+    recipeList: [
+        Passwordless.init({
+            contactMethod: "PHONE",
+        }),
+        Session.init(),
+    ],
+});
+
+function App() {
+    let [showSessionExpiredPopup, updateShowSessionExpiredPopup] = useState(false);
+
+    return (
+        <div className="App">
+            <Router>
+                <div className="fill">
+                    <Routes>
+                        {/* This shows the login UI on "/auth" route */}
+                        {getSuperTokensRoutesForReactRouterDom(require("react-router-dom"))}
+
+                        <Route
+                            path="/"
+                            element={
+                                /* This protects the "/" route so that it shows 
+                                <Home /> only if the user is logged in.
+                                Else it redirects the user to "/auth" */
+                                <Passwordless.PasswordlessAuth
+                                    onSessionExpired={() => {
+                                        updateShowSessionExpiredPopup(true);
+                                    }}>
+                                    <Home />
+                                    {showSessionExpiredPopup && <SessionExpiredPopup />}
+                                </Passwordless.PasswordlessAuth>
+                            }
+                        />
+                    </Routes>
+                </div>
+                <Footer />
+            </Router>
+        </div>
+    );
+}
+
+export default App;
