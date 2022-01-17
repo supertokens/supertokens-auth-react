f0dbc166d2506820b56de408c671b95096e416ee
Rishabh <rishabh@supertokens.io>
2022-01-17 16:34:43 +0530

    updates connection uri in passwordless demo app

--- examples/with-passwordless/api-server.js
+++ examples/with-passwordless/api-server.js
@@ -17,7 +17,7 @@ supertokens.init({
     framework: "express",
     supertokens: {
         // TODO: This is a core hosted for demo purposes. You can use this, but make sure to change it to your core instance URI eventually.
-        connectionURI: "http://localhost:3567",
+        connectionURI: "https://try.supertokens.io",
         apiKey: "<REQUIRED FOR MANAGED SERVICE, ELSE YOU CAN REMOVE THIS FIELD>",
     },
     appInfo: {
