201ef54f25f44c1c4ae1f512e4c623892139e59c
Rishabh <rishabh@supertokens.io>
2022-01-25 12:57:32 +0530

    pushes test OAuth credentials to repo

---
+++ test/.env.js
@@ -0,0 +1,12 @@
+// Github
+process.env.GITHUB_EMAIL = "bradparishdoh@gmail.com";
+process.env.GITHUB_PASSWORD = "ow93jduDSje00asd";
+// Google
+process.env.GOOGLE_EMAIL = "bradparishdoh@gmail.com";
+process.env.GOOGLE_PASSWORD = "ow93jduDSje00asd";
+// Facebook
+process.env.FACEBOOK_EMAIL = "bradparishdoh@gmail.com";
+process.env.FACEBOOK_PASSWORD = "ow93jduDSje00asd";
+// Auth0
+process.env.AUTH0_EMAIL = "bradparishdoh@gmail.com";
+process.env.AUTH0_PASSWORD = "ow93jduDSje00asd";
