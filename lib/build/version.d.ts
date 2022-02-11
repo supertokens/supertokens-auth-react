dbf0c63740ed31ddef97a5c7a80f6aaaddbf52a8
Rishabh Poddar <rishabh.poddar@gmail.com>
2022-02-11 19:19:59 +0530

    fix: Routing fix + thirdparty email verification fix (#388)
    * uses useMemo instead of state in routing component
    
    * adds a few console logs for testing
    
    * fixes bug of missing email verification config normalising in third party recipe config
    
    * further optimises routing component logic
    
    * fixes tests
    
    Co-authored-by: Rishabh <rishabh@supertokens.io>

--- lib/build/version.d.ts
+++ lib/build/version.d.ts
@@ -1,2 +1,2 @@
-export declare const package_version = "0.18.6";
+export declare const package_version = "0.18.7";
 export declare const supported_fdi: string[];
--- lib/ts/version.ts
+++ lib/ts/version.ts
@@ -12,5 +12,5 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-export const package_version = "0.18.6";
+export const package_version = "0.18.7";
 export const supported_fdi = ["1.8", "1.9", "1.10", "1.11", "1.12"];
