7fad44b601f089fe676a543ac4dab4ab2e1c3651
Rishabh <rishabh@supertokens.io>
2022-02-11 12:08:06 +0530

    further optimises routing component logic

--- lib/ts/components/routingComponent.tsx
+++ lib/ts/components/routingComponent.tsx
@@ -1,35 +1,13 @@
-// import React from "react";
-// import SuperTokens from "../superTokens";
-// import NormalisedURLPath from "../normalisedURLPath";
-
-// export function RoutingComponent(props: { supertokensInstance: SuperTokens; path: string }): JSX.Element | null {
-//     const componentToRender = React.useMemo(() => {
-//         return props.supertokensInstance.getMatchingComponentForRouteAndRecipeId(new NormalisedURLPath(props.path));
-//     }, [props]);
-
-//     const history = props.supertokensInstance.getReactRouterDomWithCustomHistory()?.useHistoryCustom();
-
-//     if (componentToRender === undefined) {
-//         return null;
-//     }
-
-//     return <componentToRender.component history={history} />;
-// }
-
 import React from "react";
 import SuperTokens from "../superTokens";
-import { ComponentWithRecipeAndMatchingMethod } from "../types";
 import NormalisedURLPath from "../normalisedURLPath";
 
 export function RoutingComponent(props: { supertokensInstance: SuperTokens; path: string }): JSX.Element | null {
-    const [componentToRender, setComponentToRender] = React.useState<ComponentWithRecipeAndMatchingMethod | undefined>(
-        undefined
-    );
-    React.useEffect(() => {
-        setComponentToRender(
-            props.supertokensInstance.getMatchingComponentForRouteAndRecipeId(new NormalisedURLPath(props.path))
-        );
-    }, [props]);
+    const stInstance = props.supertokensInstance;
+    const path = props.path;
+    const componentToRender = React.useMemo(() => {
+        return stInstance.getMatchingComponentForRouteAndRecipeId(new NormalisedURLPath(path));
+    }, [stInstance, path]);
 
     const history = props.supertokensInstance.getReactRouterDomWithCustomHistory()?.useHistoryCustom();
 
@@ -39,3 +17,29 @@ export function RoutingComponent(props: { supertokensInstance: SuperTokens; path
 
     return <componentToRender.component history={history} />;
 }
+
+// import React from "react";
+// import SuperTokens from "../superTokens";
+// import { ComponentWithRecipeAndMatchingMethod } from "../types";
+// import NormalisedURLPath from "../normalisedURLPath";
+
+// export function RoutingComponent(props: { supertokensInstance: SuperTokens; path: string }): JSX.Element | null {
+//     const stInstance = props.supertokensInstance;
+//     const path = props.path;
+//     const [componentToRender, setComponentToRender] = React.useState<ComponentWithRecipeAndMatchingMethod | undefined>(
+//         undefined
+//     );
+//     React.useEffect(() => {
+//         setComponentToRender(
+//             stInstance.getMatchingComponentForRouteAndRecipeId(new NormalisedURLPath(path))
+//         );
+//     }, [stInstance, path]);
+
+//     const history = props.supertokensInstance.getReactRouterDomWithCustomHistory()?.useHistoryCustom();
+
+//     if (componentToRender === undefined) {
+//         return null;
+//     }
+
+//     return <componentToRender.component history={history} />;
+// }
