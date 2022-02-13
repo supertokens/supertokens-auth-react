aa2403e32ed56e61899a0f318c5df714e46af771
Rishabh <rishabh@supertokens.io>
2022-02-13 19:33:41 +0530

    fixes i18n example

--- examples/with-i18next/src/App.js
+++ examples/with-i18next/src/App.js
@@ -24,9 +24,21 @@ i18next.init({
     resources: {
         en: {
             translation: {
-                BRANDING_POWERED_BY: "Powered by",
-                SOMETHING_WENT_WRONG_ERROR: "Something went wrong. Please try again",
+                THIRD_PARTY_EMAIL_PASSWORD_SIGN_IN_AND_UP_DIVIDER_OR: "or",
+                THIRD_PARTY_SIGN_IN_AND_UP_HEADER_TITLE: "Sign Up / Sign In",
 
+                THIRD_PARTY_SIGN_IN_UP_FOOTER_START: "By continuing, you agree to our ",
+                THIRD_PARTY_SIGN_IN_UP_FOOTER_TOS: "Terms of Service",
+                THIRD_PARTY_SIGN_IN_UP_FOOTER_AND: " and ",
+                THIRD_PARTY_SIGN_IN_UP_FOOTER_PP: "Privacy Policy",
+                THIRD_PARTY_SIGN_IN_UP_FOOTER_END: "",
+                THIRD_PARTY_PROVIDER_DEFAULT_BTN_START: "Continue with ",
+                THIRD_PARTY_PROVIDER_DEFAULT_BTN_END: "",
+
+                THIRD_PARTY_ERROR_NO_EMAIL: "Could not retrieve email. Please try a different method.",
+                BRANDING_POWERED_BY_START: "Powered by ",
+                BRANDING_POWERED_BY_END: "",
+                SOMETHING_WENT_WRONG_ERROR: "Something went wrong. Please try again",
                 EMAIL_VERIFICATION_RESEND_SUCCESS: "Email resent",
                 EMAIL_VERIFICATION_SEND_TITLE: "Verify your email address",
                 EMAIL_VERIFICATION_SEND_DESC_START: "",
@@ -41,16 +53,11 @@ i18next.init({
                 EMAIL_VERIFICATION_ERROR_TITLE: "Something went wrong",
                 EMAIL_VERIFICATION_ERROR_DESC:
                     "We encountered an unexpected error. Please contact support for assistance",
+                EMAIL_PASSWORD_EMAIL_LABEL: "Email",
+                EMAIL_PASSWORD_EMAIL_PLACEHOLDER: "Email address",
 
-                THIRD_PARTY_SIGN_IN_AND_UP_HEADER_TITLE: "Sign Up / Sign In",
-
-                THIRD_PARTY_SIGN_IN_UP_FOOTER_START: "By continuing, you agree to our",
-                THIRD_PARTY_SIGN_IN_UP_FOOTER_TOS: "Terms of Service",
-                THIRD_PARTY_SIGN_IN_UP_FOOTER_AND: " and ",
-                THIRD_PARTY_SIGN_IN_UP_FOOTER_PP: "Privacy Policy",
-                THIRD_PARTY_SIGN_IN_UP_FOOTER_END: "",
-                THIRD_PARTY_PROVIDER_DEFAULT_BTN_START: "Continue with ",
-                THIRD_PARTY_PROVIDER_DEFAULT_BTN_END: "",
+                EMAIL_PASSWORD_PASSWORD_LABEL: "Password",
+                EMAIL_PASSWORD_PASSWORD_PLACEHOLDER: "Password",
 
                 EMAIL_PASSWORD_SIGN_IN_HEADER_TITLE: "Sign In",
                 EMAIL_PASSWORD_SIGN_IN_HEADER_SUBTITLE_START: "Not registered yet?",
@@ -64,14 +71,13 @@ i18next.init({
                 EMAIL_PASSWORD_SIGN_UP_HEADER_SUBTITLE_START: "Already have an account?",
                 EMAIL_PASSWORD_SIGN_UP_HEADER_SUBTITLE_SIGN_IN_LINK: "Sign In",
                 EMAIL_PASSWORD_SIGN_UP_HEADER_SUBTITLE_END: "",
-                EMAIL_PASSWORD_SIGN_UP_FOOTER_START: "By continuing, you agree to our",
+                EMAIL_PASSWORD_SIGN_UP_FOOTER_START: "By continuing, you agree to our ",
                 EMAIL_PASSWORD_SIGN_UP_FOOTER_TOS: "Terms of Service",
                 EMAIL_PASSWORD_SIGN_UP_FOOTER_AND: " and ",
                 EMAIL_PASSWORD_SIGN_UP_FOOTER_PP: "Privacy Policy",
                 EMAIL_PASSWORD_SIGN_UP_FOOTER_END: "",
                 EMAIL_PASSWORD_SIGN_UP_SUBMIT_BTN: "SIGN UP",
 
-                EMAIL_PASSWORD_EMAIL_NON_STRING: "Email must be of type string",
                 EMAIL_PASSWORD_EMAIL_ALREADY_EXISTS: "This email already exists. Please sign in instead",
 
                 EMAIL_PASSWORD_RESET_HEADER_TITLE: "Reset your password",
@@ -84,13 +90,27 @@ i18next.init({
                 EMAIL_PASSWORD_RESET_SUBMIT_PW_SUCCESS_DESC: "Your password has been updated successfully",
                 EMAIL_PASSWORD_RESET_SUBMIT_PW_SUCCESS_SIGN_IN_BTN: "SIGN IN",
 
+                EMAIL_PASSWORD_NEW_PASSWORD_LABEL: "New password",
+                EMAIL_PASSWORD_NEW_PASSWORD_PLACEHOLDER: "New password",
+                EMAIL_PASSWORD_CONFIRM_PASSWORD_LABEL: "Confirm password",
+                EMAIL_PASSWORD_CONFIRM_PASSWORD_PLACEHOLDER: "Confirm your password",
+
                 EMAIL_PASSWORD_RESET_SUBMIT_PW_HEADER_TITLE: "Change your password",
                 EMAIL_PASSWORD_RESET_SUBMIT_PW_HEADER_SUBTITLE: "Enter a new password below to change your password",
-                EMAIL_PASSWORD_RESET_SUBMIT_PW_CHANGE_PW_BTN: "Change password",
+                EMAIL_PASSWORD_RESET_SUBMIT_PW_CHANGE_PW_BTN: "CHANGE PASSWORD",
                 EMAIL_PASSWORD_RESET_PASSWORD_INVALID_TOKEN_ERROR: "Invalid password reset token",
 
-                THIRD_PARTY_EMAIL_PASSWORD_SIGN_IN_AND_UP_HEADER_TITLE: "Sign Up or Log In",
-                THIRD_PARTY_EMAIL_PASSWORD_SIGN_IN_AND_UP_DIVIDER_OR: "or",
+                ERROR_EMAIL_NON_STRING: "Email must be of type string",
+                ERROR_EMAIL_INVALID: "Email is invalid",
+
+                ERROR_PASSWORD_NON_STRING: "Password must be of type string",
+                ERROR_PASSWORD_TOO_SHORT: "Password must contain at least 8 characters, including a number",
+                ERROR_PASSWORD_TOO_LONG: "Password's length must be lesser than 100 characters",
+                ERROR_PASSWORD_NO_ALPHA: "Password must contain at least one alphabet",
+                ERROR_PASSWORD_NO_NUM: "Password must contain at least one number",
+                ERROR_CONFIRM_PASSWORD_NO_MATCH: "Confirmation password doesn't match",
+
+                ERROR_NON_OPTIONAL: "Field is not optional",
             },
         },
         hu: {
@@ -120,7 +140,7 @@ export function getWebsiteDomain() {
 
 SuperTokens.init({
     languageTranslations: {
-        translationFunc: i18next.t,
+        translationFunc: i18next.t.bind(i18next),
     },
     appInfo: {
         appName: "SuperTokens Demo App", // TODO: Your app name
