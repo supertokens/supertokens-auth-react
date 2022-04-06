d43a336341d606eb5c30464fda462cc811855171
icode247 <zionekekenta@gmail.com>
2022-04-06 16:23:54 +0100

    modified the api-server

--- examples/with-emailpassword-vercel/api/api-server.js
+++ examples/with-emailpassword-vercel/api/api-server.js
@@ -8,22 +8,24 @@ let { verifySession } = require("supertokens-node/recipe/session/framework/expre
 let { middleware, errorHandler } = require("supertokens-node/framework/express");
 let EmailPassword = require("supertokens-node/recipe/emailpassword");
 
-const apiPort = process.env.REACT_APP_API_PORT || 3001;
-const apiDomain = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
-const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
-const websiteDomain = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+// const apiPort = process.env.REACT_APP_API_PORT || 3001;
+// const apiDomain = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+// const websitePort = process.env.VERCEL_URL || 3000;
+// const websiteDomain = process.env.VERCEL_URL || `http://localhost:${websitePort}`;
 
 supertokens.init({
     framework: "express",
     supertokens: {
         // TODO: This is a core hosted for demo purposes. You can use this, but make sure to change it to your core instance URI eventually.
         connectionURI: "https://try.supertokens.com",
-        apiKey: "<REQUIRED FOR MANAGED SERVICE, ELSE YOU CAN REMOVE THIS FIELD>",
+        //apiKey: "<REQUIRED FOR MANAGED SERVICE, ELSE YOU CAN REMOVE THIS FIELD>",
     },
     appInfo: {
         appName: "SuperTokens Demo App", // TODO: Your app name
-        apiDomain, // TODO: Change to your app's API domain
-        websiteDomain, // TODO: Change to your app's website domain
+        apiDomain: process.env.VERCEL_URL,
+        websiteDomain: process.env.VERCEL_URL,
+        apiBasePath: "/api/auth",
+        websiteBasePath: "/auth",
     },
     recipeList: [EmailPassword.init(), Session.init()],
 });
@@ -32,7 +34,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: websiteDomain, // TODO: Change to your app's website domain
+        origin: process.env.VERCEL_URL, // TODO: Change to your app's website domain
         allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
         methods: ["GET", "PUT", "POST", "DELETE"],
         credentials: true,
