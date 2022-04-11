b710fabd0d5e2267d698e52dde36d30470eeadee
Ekekenta Odionyenfe .C <60361247+icode247@users.noreply.github.com>
2022-04-11 05:55:14 +0100

    With emailpassword vercel (#412)
    * created the demo app
    
    * created the api folder for the backend
    
    * modified the frontend
    
    * made changes on the api
    
    * modified the api-server
    
    * modified the api-server
    
    * modified frontend and api
    
    * updates on README.md
    
    * update on README.md
    
    * update on REAME.md
    
    * update on REAME.md
    
    * fixed some issues
    
    * modified README.md
    
    * modified README.md
    
    * modified README
    
    * Update examples/with-emailpassword-vercel/README.md
    
    * modiefied the project
    
    * updated the Vercel link
    
    * few fixes
    
    Co-authored-by: Rishabh Poddar <rishabh.poddar@gmail.com>

---
+++ examples/with-emailpassword-vercel/README.md
@@ -0,0 +1,55 @@
+![SuperTokens banner](https://raw.githubusercontent.com/supertokens/supertokens-logo/master/images/Artboard%20%E2%80%93%2027%402x.png)
+
+# SuperTokens EmailPassword Vercel Demo app
+
+This demo app demonstrates deploying a react + nodejs express app on Vercel.
+
+## Project structure & Parameters
+
+-   The frontend code is located in the `src` folder.
+-   The backend API is in the `api/index.js` file .
+-   The `vercel.json` file contains the configurations that allow Vercel to rewrite to push all traffic on `/api` routes to the backend.
+
+## Run application locally
+
+Follow the steps outlined below to run the application locally:
+
+1. Change directory to the **with-emailpassword-vercel** folder.
+
+
+        cd supertokens-auth-react/examples/with-emailpassword-vercel
+
+2. Run the command below to install the project dependencies.
+
+  
+        npm install 
+
+4. Start the application with the command below:
+      
+        npm start
+ 
+
+## Deploy to Vercel
+
+To deploy this demo application on Vercel, follow the steps outlined below:
+1. Clone or download this project.
+
+2. Change directory to the **with-emailpassword-vercel** folder.
+     
+
+       cd supertokens-auth-react/examples/with-emailpassword-vercel
+           
+
+3. Run the `vercel` command, to deploy the project.
+
+       vercel
+
+Also, this project is [live](https://with-emailpassword-vercel-d1sj3fk2d-icode247.vercel.app/) on Vercel, feel free to test out the project. 
+
+## Author
+
+Created with :heart: by the folks at supertokens.com.
+
+## License
+
+This project is licensed under the Apache 2.0 license.
--- examples/with-emailpassword-vercel/api/index.js
+++ examples/with-emailpassword-vercel/api/index.js
@@ -9,21 +9,23 @@ let { middleware, errorHandler } = require("supertokens-node/framework/express")
 let EmailPassword = require("supertokens-node/recipe/emailpassword");
 
 const apiPort = process.env.REACT_APP_API_PORT || 3001;
-const apiDomain = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+const apiDomain = process.env.VERCEL_URL !== undefined ? process.env.VERCEL_URL : (process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`)
 const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
-const websiteDomain = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+const websiteDomain = process.env.VERCEL_URL !== undefined ? process.env.VERCEL_URL : (process.env.REACT_APP_API_URL|| `http://localhost:${websitePort}`);
 
 supertokens.init({
     framework: "express",
     supertokens: {
         // TODO: This is a core hosted for demo purposes. You can use this, but make sure to change it to your core instance URI eventually.
         connectionURI: "https://try.supertokens.com",
-        // apiKey: "<REQUIRED FOR MANAGED SERVICE, ELSE YOU CAN REMOVE THIS FIELD>",
+        //apiKey: "<REQUIRED FOR MANAGED SERVICE, ELSE YOU CAN REMOVE THIS FIELD>",
     },
     appInfo: {
         appName: "SuperTokens Demo App", // TODO: Your app name
-        apiDomain, // TODO: Change to your app's API domain
-        websiteDomain, // TODO: Change to your app's website domain
+        apiDomain,
+        websiteDomain,
+        apiBasePath: "/api/auth",
+        websiteBasePath: "/auth",
     },
     recipeList: [EmailPassword.init(), Session.init()],
 });
@@ -39,6 +41,7 @@ app.use(
     })
 );
 
+//add morgan to log HTTP requests
 app.use(morgan("dev"));
 app.use(
     helmet({
@@ -48,7 +51,7 @@ app.use(
 app.use(middleware());
 
 // custom API that requires session verification
-app.get("/sessioninfo", verifySession(), async (req, res) => {
+app.get("/api/sessioninfo", verifySession(), async (req, res) => {
     let session = req.session;
     res.send({
         sessionHandle: session.getHandle(),
@@ -59,8 +62,11 @@ app.get("/sessioninfo", verifySession(), async (req, res) => {
 
 app.use(errorHandler());
 
+
 app.use((err, req, res, next) => {
     res.status(500).send("Internal error: " + err.message);
 });
 
 app.listen(apiPort, () => console.log(`API Server listening on port ${apiPort}`));
+
+module.exports = app;
