a421914c9710941b6f4ba2c53054c5c91c69b868
jscyo <joelsteve97@hotmail.com>
2022-05-17 16:02:45 +0530

    fix

--- examples/with-supabase/config/appInfo.ts
+++ examples/with-supabase/config/appInfo.ts
@@ -5,7 +5,7 @@ const apiBasePath = "/api/auth/";
 export const websiteDomain = process.env.APP_URL || process.env.NEXT_PUBLIC_APP_URL || `http://localhost:${port}`;
 
 export const appInfo = {
-    appName: "SuperTokens and Supabase Demo app",
+    appName: "SuperTokens and Supabase Demo App",
     websiteDomain,
     apiDomain: websiteDomain,
     apiBasePath,
