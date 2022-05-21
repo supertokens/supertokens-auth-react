bdcd8c0eb89947da271ac73793f8b7ed283f2086
rishabhpoddar <rishabh.poddar@gmail.com>
2022-05-21 11:58:08 +0530

    adds emailpassword login fully

--- examples/with-phone-password/api-server/index.ts
+++ examples/with-phone-password/api-server/index.ts
@@ -4,7 +4,7 @@ import supertokens from "supertokens-node";
 import Session from "supertokens-node/recipe/session";
 import { verifySession } from "supertokens-node/recipe/session/framework/express";
 import { middleware, errorHandler, SessionRequest } from "supertokens-node/framework/express";
-import ThirdPartyEmailPassword from "supertokens-node/recipe/thirdpartyemailpassword";
+import EmailPassword from "supertokens-node/recipe/emailpassword";
 import Passwordless from "supertokens-node/recipe/passwordless";
 // import { tpepOverride } from "./tpepOverride";
 // import { plessOverride } from "./plessOverride";
@@ -31,39 +31,7 @@ supertokens.init({
         websiteDomain, // TODO: Change to your app's website domain
     },
     recipeList: [
-        ThirdPartyEmailPassword.init({
-            providers: [
-                // We have provided you with development keys which you can use for testing.
-                // IMPORTANT: Please replace them with your own OAuth keys for production use.
-                ThirdPartyEmailPassword.Google({
-                    clientId: "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
-                    clientSecret: "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",
-                }),
-                ThirdPartyEmailPassword.Github({
-                    clientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd",
-                    clientId: "467101b197249757c71f",
-                }),
-                ThirdPartyEmailPassword.Apple({
-                    clientId: "4398792-io.supertokens.example.service",
-                    clientSecret: {
-                        keyId: "7M48Y4RYDL",
-                        privateKey:
-                            "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
-                        teamId: "YWQCXGJRJL",
-                    },
-                }),
-            ],
-            // override: {
-            //     functions: (originalImpl) => {
-            //         return tpepOverride(originalImpl);
-            //     },
-            //     emailVerificationFeature: {
-            //         functions: (originalImpl) => {
-            //             return evOverride(originalImpl);
-            //         },
-            //     },
-            // },
-        }),
+        EmailPassword.init({}),
         Passwordless.init({
             contactMethod: "EMAIL_OR_PHONE",
             flowType: "USER_INPUT_CODE_AND_MAGIC_LINK",
--- examples/with-phone-password/src/App.tsx
+++ examples/with-phone-password/src/App.tsx
@@ -1,19 +1,68 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
+import SuperTokens, { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react";
+import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
+import Session from "supertokens-auth-react/recipe/session";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import * as reactRouterDom from "react-router-dom";
+import Home from "./Home";
+import Footer from "./Footer";
+import SessionExpiredPopup from "./SessionExpiredPopup";
+
+export function getApiDomain() {
+    const apiPort = process.env.REACT_APP_API_PORT || 3001;
+    const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+    return apiUrl;
+}
+
+export function getWebsiteDomain() {
+    const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+    const websiteUrl = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+    return websiteUrl;
+}
+
+SuperTokens.init({
+    appInfo: {
+        // learn more about this on https://supertokens.com/docs/emailpassword/appinfo
+        appName: "SuperTokens Demo App",
+        apiDomain: getApiDomain(),
+        websiteDomain: getWebsiteDomain(),
+        apiBasePath: "/auth",
+        websiteBasePath: "/auth",
+    },
+    recipeList: [EmailPassword.init(), Session.init()],
+});
 
 function App() {
+    let [showSessionExpiredPopup, updateShowSessionExpiredPopup] = useState(false);
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <Router>
+                <div className="fill">
+                    <Routes>
+                        {/* This shows the login UI on "/auth" route */}
+                        {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
+
+                        <Route
+                            path="/"
+                            element={
+                                /* This protects the "/" route so that it shows 
+                <Home /> only if the user is logged in.
+                Else it redirects the user to "/auth" */
+                                <EmailPassword.EmailPasswordAuth
+                                    onSessionExpired={() => {
+                                        updateShowSessionExpiredPopup(true);
+                                    }}>
+                                    <Home />
+                                    {showSessionExpiredPopup && <SessionExpiredPopup />}
+                                </EmailPassword.EmailPasswordAuth>
+                            }
+                        />
+                    </Routes>
+                </div>
+                <Footer />
+            </Router>
         </div>
     );
 }
