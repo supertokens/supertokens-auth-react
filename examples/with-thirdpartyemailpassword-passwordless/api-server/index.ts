acf2552988f79be1812b6f4e973adf69bc9d8403
rishabhpoddar <rishabh.poddar@gmail.com>
2022-05-26 16:50:41 +0530

    fixes connectionURI for example app

--- examples/with-thirdpartyemailpassword-passwordless/api-server/index.ts
+++ examples/with-thirdpartyemailpassword-passwordless/api-server/index.ts
@@ -23,7 +23,7 @@ supertokens.init({
     framework: "express",
     supertokens: {
         // TODO: This is a core hosted for demo purposes. You can use this, but make sure to change it to your core instance URI eventually.
-        connectionURI: "http://localhost:3567",
+        connectionURI: "https://try.supertokens.com",
         apiKey: "<REQUIRED FOR MANAGED SERVICE, ELSE YOU CAN REMOVE THIS FIELD>",
     },
     appInfo: {
