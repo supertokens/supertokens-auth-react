138ddad926759f70abf8db57a4dc681b6fa2d965
rishabhpoddar <rishabh.poddar@gmail.com>
2022-05-26 12:45:34 +0530

    docs: changes comment over example API in example apps

--- examples/with-thirdparty-2fa-passwordless/api-server/index.js
+++ examples/with-thirdparty-2fa-passwordless/api-server/index.js
@@ -160,7 +160,7 @@ app.use(
 );
 app.use(middleware());
 
-// custom API that requires session verification
+// An example API that requires session verification
 app.get(
     "/sessioninfo",
     verifySession(),
