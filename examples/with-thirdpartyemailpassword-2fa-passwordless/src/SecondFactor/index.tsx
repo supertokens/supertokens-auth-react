f30fb221c7395ef783f3912e937347c57755d36d
Nemi Shah <nemishah1212@gmail.com>
2022-06-03 20:34:47 +0530

    chore: Create example app for thirdpartyemailpassword with passwordless 2fa and email verification (#475)
    * Basic setup for tpep 2fa passwordless example app
    
    * Implement example app
    
    Co-authored-by: Rishabh Poddar <rishabh.poddar@gmail.com>

---
+++ examples/with-thirdpartyemailpassword-2fa-passwordless/src/SecondFactor/index.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Passwordless from "supertokens-auth-react/recipe/passwordless";
+import Session from "supertokens-auth-react/recipe/session";
+import ThirdPartyEmailPassword from "supertokens-auth-react/lib/build/recipe/thirdpartyemailpassword";
+
+export default function SecondFactor() {
+    React.useEffect(() => {
+        // we redirect the user back to the auth screen in case
+        // their first factor auth is not done
+        async function doCheck() {
+            if (
+                !(await Session.doesSessionExist({
+                    userContext: {
+                        forceOriginalCheck: true,
+                    },
+                }))
+            ) {
+                ThirdPartyEmailPassword.redirectToAuth({
+                    redirectBack: false,
+                });
+            }
+        }
+        doCheck();
+    }, []);
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+            }}>
+            <Passwordless.SignInUp />
+            <div
+                onClick={async () => {
+                    await Session.signOut({
+                        userContext: {
+                            forceOriginalCheck: true,
+                        },
+                    });
+                    ThirdPartyEmailPassword.redirectToAuth({
+                        redirectBack: false,
+                    });
+                }}
+                style={{
+                    cursor: "pointer",
+                    color: "blue",
+                    textDecoration: "underline",
+                }}>
+                Login with another account
+            </div>
+        </div>
+    );
+}
