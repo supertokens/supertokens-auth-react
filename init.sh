fa6bb752a755c66f377a0bcefca033d0a61fa1df
Mihály Lengyel <mihaly@lengyel.tech>
2022-06-25 08:01:24 +0200

    ci: fix react16 tests (#509)
    * test: fix test scripts
    
    * test building
    
    * build: mark each step in init script
    
    * ci: fix react16 tests running
    
    * build: revert test change
    
    * ci: splitting react version tests into separate job runs
    
    * ci: better namingf for test matrix job
    
    * ci: better naming for test matrix job
    
    * test: fix skipping tests of general errors
    
    * test: fix react16 tests

--- init.sh
+++ init.sh
@@ -6,4 +6,17 @@
 # export PUPPETEER_EXECUTABLE_PATH=`which chromium`
 # And run brew install chromium --no-quarantine
 
-npm i -d --force && (cd ./test/server && npm i ) && (cd ./examples/for-tests && npm i -d --force && npm run prep) && (cd ./examples/for-tests-react-16 && npm i -d --force --legacy-peer-deps && npm run prep) && (cd ./test/with-typescript && npm i -d --force)
+echo "Install in root"
+npm i -d --force || exit $?
+
+echo "Install in test server"
+(cd ./test/server && npm i ) || exit $?
+
+echo "Install in test frontend"
+(cd ./examples/for-tests && npm i -d --force && npm run prep) || exit $? 
+
+echo "Install in test frontend for react 16"
+(cd ./examples/for-tests-react-16 && npm i -d --force --legacy-peer-deps && npm run prep) || exit $?
+
+echo "Install in type check test project"
+(cd ./test/with-typescript && npm i -d --force) || exit $?
