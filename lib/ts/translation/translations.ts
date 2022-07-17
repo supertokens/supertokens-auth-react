a90b81c3c5d92e920cd50bc7cf56743e1d2a3a8a
Morgante Pell <morgante.pell@morgante.net>
2022-07-17 14:39:43 -0700

    fix: Correct grammar for something went wrong message.

--- lib/ts/translation/translations.ts
+++ lib/ts/translation/translations.ts
@@ -2,6 +2,6 @@ export const defaultTranslationsCommon = {
     en: {
         BRANDING_POWERED_BY_START: "Powered by ",
         BRANDING_POWERED_BY_END: "",
-        SOMETHING_WENT_WRONG_ERROR: "Something went wrong. Please try again",
+        SOMETHING_WENT_WRONG_ERROR: "Something went wrong. Please try again.",
     },
 };
