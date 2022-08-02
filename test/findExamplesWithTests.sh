8da3f756ec79dfd20bea8edce4ce119f03f80bdd
Mihaly Lengyel <mihaly@lengyel.tech>
2022-08-02 22:24:51 +0200

    ci: test matrix build in GH

--- test/findExamplesWithTests.sh
+++ test/findExamplesWithTests.sh
@@ -1,9 +1,9 @@
-testAbleExamples=""
+matrixEntries=""
 delim="{examplePath: "
 while IFS=  read -r -d $'\0'; do
-    testAbleExamples+=$delim\"$(echo "$REPLY" | sed 's/\/test$//' )\";       # or however you want to process each file
-    delim="}, "$delim
+    matrixEntries+=$delim\"$(echo "$REPLY" | sed 's/\/test$//' )\""}";       # or however you want to process each file
+    delim=", "$delim
 done < <(find ./examples -name node_modules -prune -or -path "*/test" -print0)
 
-testAbleExamples="[$testAbleExamples]"
-echo $testAbleExamples
+matrixEntries="[$matrixEntries]"
+echo $matrixEntries
