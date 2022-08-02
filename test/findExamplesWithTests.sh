87d9ab6bb4807106b1ebc99872797b693954672d
Mihaly Lengyel <mihaly@lengyel.tech>
2022-08-02 22:12:03 +0200

    ci: test matrix build in GB

--- test/findExamplesWithTests.sh
+++ test/findExamplesWithTests.sh
@@ -1,8 +1,8 @@
 testAbleExamples=""
-delim=""
+delim="{examplePath: "
 while IFS=  read -r -d $'\0'; do
     testAbleExamples+=$delim\"$(echo "$REPLY" | sed 's/\/test$//' )\";       # or however you want to process each file
-    delim=", "
+    delim="}, "$delim
 done < <(find ./examples -name node_modules -prune -or -path "*/test" -print0)
 
 testAbleExamples="[$testAbleExamples]"
