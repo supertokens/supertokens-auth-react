8e64dd419448a8075a05c8744af018de139c9b65
Mihaly Lengyel <mihaly@lengyel.tech>
2022-08-04 03:54:40 +0200

    build: fix finding testable examples

--- test/findExamplesWithTests.sh
+++ test/findExamplesWithTests.sh
@@ -1,7 +1,7 @@
 matrixEntries=""
 delim=""
 while IFS=  read -r -d $'\0'; do
-    matrixEntries+=$delim\""{examplePath: "$(echo "$REPLY" | sed 's/\/test$//' )\""}";       # or however you want to process each file
+    matrixEntries+=$delim"{\"examplePath\": \""$(echo "$REPLY" | sed 's/\/test$//' )\""}";       # or however you want to process each file
     delim=", "
 done < <(find ./examples -name node_modules -prune -or -path "*/test" -print0)
 
