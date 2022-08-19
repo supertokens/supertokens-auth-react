c6aca6f0ddf4a92e5a3104c356e51aa74d41c369
rishabhpoddar <rishabh.poddar@gmail.com>
2022-08-19 16:20:24 +0530

    creates skeleton

---
+++ examples/with-cli-login/README.md
@@ -0,0 +1,35 @@
+![SuperTokens banner](https://raw.githubusercontent.com/supertokens/supertokens-logo/master/images/Artboard%20%E2%80%93%2027%402x.png)
+
+# SuperTokens login to CLI
+
+This demo app demonstrates how we can implement sign in for a CLI client in which it opens the browser and asks you to login there if not logged in already.
+
+## Project setup
+
+Clone the repo, enter the directory, and use `npm` to install the project dependencies:
+
+```bash
+git clone https://github.com/supertokens/supertokens-auth-react
+cd supertokens-auth-react/examples/with-cli-login
+npm install
+```
+
+## Run the demo app
+
+This compiles and serves the React app and starts the backend API server on port 3001.
+
+```bash
+npm run start
+```
+
+The app will start on `http://localhost:3000`
+
+Then you can start the cli took by running `./startCLI` command which will prompt you to login.
+
+## Author
+
+Created with :heart: by the folks at supertokens.com.
+
+## License
+
+This project is licensed under the Apache 2.0 license.
---
+++ examples/with-cli-login/api-server/index.ts
@@ -0,0 +1,94 @@
+import express from "express";
+import cors from "cors";
+import supertokens from "supertokens-node";
+import Session from "supertokens-node/recipe/session";
+import { verifySession } from "supertokens-node/recipe/session/framework/express";
+import { middleware, errorHandler, SessionRequest } from "supertokens-node/framework/express";
+import ThirdPartyEmailPassword from "supertokens-node/recipe/thirdpartyemailpassword";
+import Passwordless from "supertokens-node/recipe/passwordless";
+import UserMetadata from "supertokens-node/recipe/usermetadata";
+require("dotenv").config();
+
+const apiPort = process.env.REACT_APP_API_PORT || 3001;
+const apiDomain = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+const websiteDomain = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+
+export const emailVerificationOn = true;
+
+supertokens.init({
+    framework: "express",
+    supertokens: {
+        // TODO: This is a core hosted for demo purposes. You can use this, but make sure to change it to your core instance URI eventually.
+        connectionURI: "https://try.supertokens.com",
+        apiKey: "<REQUIRED FOR MANAGED SERVICE, ELSE YOU CAN REMOVE THIS FIELD>",
+    },
+    appInfo: {
+        appName: "SuperTokens Demo App",
+        apiDomain,
+        websiteDomain,
+    },
+    recipeList: [
+        ThirdPartyEmailPassword.init({
+            providers: [
+                // We have provided you with development keys which you can use for testing.
+                // IMPORTANT: Please replace them with your own OAuth keys for production use.
+                ThirdPartyEmailPassword.Google({
+                    clientId: "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
+                    clientSecret: "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",
+                }),
+                ThirdPartyEmailPassword.Github({
+                    clientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd",
+                    clientId: "467101b197249757c71f",
+                }),
+                ThirdPartyEmailPassword.Apple({
+                    clientId: "4398792-io.supertokens.example.service",
+                    clientSecret: {
+                        keyId: "7M48Y4RYDL",
+                        privateKey:
+                            "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
+                        teamId: "YWQCXGJRJL",
+                    },
+                }),
+            ],
+        }),
+        UserMetadata.init(),
+        Passwordless.init({
+            contactMethod: "EMAIL",
+            flowType: "MAGIC_LINK",
+        }),
+        Session.init(),
+    ],
+});
+
+const app = express();
+
+app.use(
+    cors({
+        origin: websiteDomain,
+        allowedHeaders: ["content-type", ...supertokens.getAllCORSHeaders()],
+        methods: ["GET", "PUT", "POST", "DELETE"],
+        credentials: true,
+    })
+);
+
+app.use(middleware());
+
+// An example API that requires session verification
+app.get("/sessioninfo", verifySession(), async (req: SessionRequest, res) => {
+    let session = req.session!;
+    res.send({
+        sessionHandle: session.getHandle(),
+        userId: session.getUserId(),
+        accessTokenPayload: session.getAccessTokenPayload(),
+    });
+});
+
+app.use(errorHandler());
+
+app.use((err: any, req: any, res: any, next: any) => {
+    console.log(err);
+    res.status(500).send("Internal error: " + err.message);
+});
+
+app.listen(apiPort, () => console.log(`API Server listening on port ${apiPort}`));
