7dd4fa25ea05841295ef4310de99a4ae8a73c928
Nemi Shah <nemishah1212@gmail.com>
2023-02-24 12:42:50 +0530

    Fix fonts

--- lib/ts/recipe/thirdparty/components/themes/themeBase.tsx
+++ lib/ts/recipe/thirdparty/components/themes/themeBase.tsx
@@ -28,7 +28,7 @@ export const ThemeBase: React.FC<
             {children}
             {loadDefaultFont && (
                 <link
-                    href="//fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700"
+                    href="//fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap"
                     rel="stylesheet"
                     type="text/css"></link>
             )}
