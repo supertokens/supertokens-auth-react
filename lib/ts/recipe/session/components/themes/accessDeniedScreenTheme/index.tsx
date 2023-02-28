82802b8c1b052f163c47c90af3ff73aeaa725df3
Alisher <alisher@supertokens.com>
2023-02-28 11:51:20 +0600

    fix: adjust access-denied page

--- lib/ts/recipe/session/components/themes/accessDeniedScreenTheme/index.tsx
+++ lib/ts/recipe/session/components/themes/accessDeniedScreenTheme/index.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/jsx-no-literals */
 import React from "react";
 
-import { signOut } from "../../..";
+// import { signOut } from "../../..";
 import { withOverride } from "../../../../../components/componentOverride/withOverride";
 import { hasFontDefined } from "../../../../../styles/styles";
 import SuperTokens from "../../../../../superTokens";
 import { useTranslation } from "../../../../../translation/translationContext";
+import { useUserContext } from "../../../../../usercontext";
+import Session from "../../../recipe";
 import { BackButton } from "../../library/backButton";
 import { LogoutButton } from "../../library/logoutButton";
 import { ThemeBase } from "../themeBase";
@@ -14,23 +16,33 @@ import type { AccessDeniedThemeProps } from "../../../types";
 import type { FC } from "react";
 
 const AccessDeniedScreen: FC<AccessDeniedThemeProps> = () => {
+    const userContext = useUserContext();
     const t = useTranslation();
     const history = SuperTokens.getReactRouterDomWithCustomHistory()?.useHistoryCustom();
+
     const onLogout = async () => {
         try {
-            await signOut();
+            await Session.getInstanceOrThrow().signOut({ userContext });
         } catch (error) {
         } finally {
             history.navigate("/auth");
         }
     };
+
+    const onBackButtonClicked = () => {
+        return SuperTokens.getInstanceOrThrow().redirectToAuth({
+            show: "signin",
+            history,
+            redirectBack: false,
+        });
+    };
+
     return (
-        <div>
-            <LogoutButton onClick={onLogout} />
-            <div data-supertokens="center">
-                <div data-supertokens="headerTitle">{t("ACCESS_DENIED")}</div>
-                // eslint-disable-next-line no-console
-                <BackButton onClick={() => null} />
+        <div data-supertokens="center">
+            <div data-supertokens="headerTitle">{t("ACCESS_DENIED")}</div>
+            <div data-supertokens="buttons">
+                <BackButton onClick={onBackButtonClicked} />
+                <LogoutButton onClick={onLogout} />
             </div>
         </div>
     );
