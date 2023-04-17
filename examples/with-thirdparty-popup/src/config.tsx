8308a07d807193d634a983a93b2a4fce374424a4
Sattvik Chakravarthy <sattvik@gmail.com>
2023-04-17 11:39:14 +0530

    fix: thirdparty popup example

---
+++ examples/with-thirdparty-popup/src/config.tsx
@@ -0,0 +1,48 @@
+import ThirdParty, { Google, Github, Apple } from "supertokens-auth-react/recipe/thirdparty";
+import Session from "supertokens-auth-react/recipe/session";
+
+export function getApiDomain() {
+    const apiPort = process.env.REACT_APP_API_PORT || 3001;
+    const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+    return apiUrl;
+}
+
+export function getWebsiteDomain() {
+    const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+    const websiteUrl = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+    return websiteUrl;
+}
+
+export const SuperTokensConfig = {
+    appInfo: {
+        appName: "SuperTokens Demo App",
+        apiDomain: getApiDomain(),
+        websiteDomain: getWebsiteDomain(),
+    },
+    // recipeList contains all the modules that you want to
+    // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
+    recipeList: [
+        ThirdParty.init({
+            signInAndUpFeature: {
+                providers: [Github.init(), Google.init(), Apple.init()],
+            },
+            override: {
+                functions: (originalImplementation) => {
+                    return {
+                        ...originalImplementation,
+                        signInAndUp: async (input) => {
+                            const result = await originalImplementation.signInAndUp(input);
+                            window.close();
+                            return result;
+                        },
+                    }
+                },
+            }
+        }),
+        Session.init(),
+    ],
+};
+
+export const recipeDetails = {
+    docsLink: "https://supertokens.com/docs/thirdparty/introduction",
+};
