f25ff21d62e347f4da00e09385d5403644869f48
Mihaly Lengyel <mihaly@lengyel.tech>
2023-05-02 19:01:57 +0200

    chore: update google-onetap example to use new interface

--- examples/with-thirdparty-google-onetap/frontend/src/App.tsx
+++ examples/with-thirdparty-google-onetap/frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
-import SuperTokens, { SuperTokensWrapper, getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react";
+import SuperTokens, { SuperTokensWrapper } from "supertokens-auth-react";
+import { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react/ui";
 import { SessionAuth } from "supertokens-auth-react/recipe/session";
 import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Home";
 import { SuperTokensConfig } from "./config";
 import { ThirdpartyComponentsOverrideProvider } from "supertokens-auth-react/recipe/thirdparty";
+import { ThirdPartyPreBuiltUI } from "supertokens-auth-react/recipe/thirdparty/prebuiltui";
 import GoogleOneTapLogin from "./google-one-tap";
-import { useNavigate } from "react-router-dom";
 
 import ThirdParty from "supertokens-auth-react/recipe/thirdparty";
 
@@ -69,7 +70,9 @@ function App() {
                         <div className="fill">
                             <Routes>
                                 {/* This shows the login UI on "/auth" route */}
-                                {getSuperTokensRoutesForReactRouterDom(require("react-router-dom"))}
+                                {getSuperTokensRoutesForReactRouterDom(require("react-router-dom"), [
+                                    ThirdPartyPreBuiltUI,
+                                ])}
 
                                 <Route
                                     path="/"
