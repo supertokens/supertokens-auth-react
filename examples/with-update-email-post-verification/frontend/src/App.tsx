f25d265d717677d31fa4a2caa48ac3939ce802f3
rishabhpoddar <rishabh.poddar@gmail.com>
2023-05-31 11:34:23 +0530

    changes readme of demo app

--- examples/with-update-email-post-verification/frontend/src/App.tsx
+++ examples/with-update-email-post-verification/frontend/src/App.tsx
@@ -24,7 +24,7 @@ function App() {
                                     /* This protects the "/" route so that it shows
                                   <Home /> only if the user is logged in.
                                   Else it redirects the user to "/auth" */
-                                    <SessionAuth userContext={{}}>
+                                    <SessionAuth>
                                         <Home />
                                     </SessionAuth>
                                 }
