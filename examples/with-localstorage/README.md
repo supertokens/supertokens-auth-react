a03b9d68e8603157836b3b3c834c19815af1c65b
Rishabh Poddar <rishabh.poddar@gmail.com>
2023-06-08 19:39:31 +0530

    adds new example app (#712)
    * adds new example app
    
    * adds tests for example app
    
    * updates
    
    * fixes example app for tests

--- examples/with-localstorage/README.md
+++ examples/with-localstorage/README.md
@@ -1,10 +1,25 @@
 ![SuperTokens banner](https://raw.githubusercontent.com/supertokens/supertokens-logo/master/images/Artboard%20%E2%80%93%2027%402x.png)
 
-# SuperTokens Demo app
+# SuperTokens Demo app - with-localstorage
 
-This demo app is now deprecated. We recommend using a built-in customization option to switch to using the Authorization header. You can check it out [here](https://supertokens.com/docs/thirdpartyemailpassword/common-customizations/sessions/token-transfer-method).
+This demo app shows you how to substitute frontend cookies storage with localstorage when using header based auth.
 
-On top of that, you can switch to only using `localStorage` by adding a custom CookieHandler.
+The main idea is that we provide a custom `cookieHandler` on the frontend's supertokens.init which will provide functions for getting and setting frontend cookies, but instead of writing to cookies, it will write to localstorage.
+
+You can find the implementation of this custom handler in `/frontend/src/cookieHandler.ts`. The function `getCookieHandler` that is exposed by this file is used in `supertokens.init` in `/frontend/src/config.ts`.
+
+You can start the demo app by running:
+
+```bash
+git clone https://github.com/supertokens/supertokens-auth-react
+cd supertokens-auth-react/examples/with-localstorage
+
+npm install
+
+npm run start
+```
+
+Once you login into the demo app, then you will see that the cookie store on the frontend no longer has the session tokens, but if you open localstorage, you will see that all the tokens are under the key `frontendCookies`.
 
 ## Author
 
---
+++ examples/with-localstorage/frontend/src/config.tsx
@@ -0,0 +1,44 @@
+import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
+import { EmailPasswordPreBuiltUI } from "supertokens-auth-react/recipe/emailpassword/prebuiltui";
+import Session from "supertokens-auth-react/recipe/session";
+import getCookieHandler from "./cookieHandler";
+import EmailVerification from "supertokens-auth-react/recipe/emailverification";
+import { EmailVerificationPreBuiltUI } from "supertokens-auth-react/recipe/emailverification/prebuiltui";
+
+export function getApiDomain() {
+    const apiPort = process.env.REACT_APP_API_PORT || 3001;
+    const apiUrl = process.env.REACT_APP_API_URL || `http://localhost:${apiPort}`;
+    return apiUrl;
+}
+
+export function getWebsiteDomain() {
+    const websitePort = process.env.REACT_APP_WEBSITE_PORT || 3000;
+    const websiteUrl = process.env.REACT_APP_WEBSITE_URL || `http://localhost:${websitePort}`;
+    return websiteUrl;
+}
+
+export const SuperTokensConfig = {
+    cookieHandler: getCookieHandler,
+    appInfo: {
+        appName: "SuperTokens Demo App",
+        apiDomain: getApiDomain(),
+        websiteDomain: getWebsiteDomain(),
+    },
+    // recipeList contains all the modules that you want to
+    // use from SuperTokens. See the full list here: https://supertokens.com/docs/guides
+    recipeList: [
+        EmailPassword.init(),
+        Session.init({
+            tokenTransferMethod: "header",
+        }),
+        EmailVerification.init({
+            mode: "REQUIRED",
+        }),
+    ],
+};
+
+export const recipeDetails = {
+    docsLink: "https://supertokens.com/docs/emailpassword/introduction",
+};
+
+export const PreBuiltUIList = [EmailPasswordPreBuiltUI, EmailVerificationPreBuiltUI];
