529aacf6e9c6e0442d543751090e7fe31fe33685
Nemi Shah <nemishah1212@gmail.com>
2023-09-04 14:59:44 +0530

    Update CICD

--- init.sh
+++ init.sh
@@ -7,16 +7,16 @@
 # And run brew install chromium --no-quarantine
 
 echo "Install in root"
-npm i -d --force || exit $?
+GIT_SSH_COMMAND="ssh" npm i -d --force || exit $?
 
 echo "Install in test server"
-(cd ./test/server && npm i ) || exit $?
+(cd ./test/server && GIT_SSH_COMMAND="ssh" npm i ) || exit $?
 
 echo "Install in test frontend"
-(cd ./examples/for-tests && npm i -d --force && npm run prep) || exit $? 
+(cd ./examples/for-tests && GIT_SSH_COMMAND="ssh" npm i -d --force && npm run prep) || exit $? 
 
 echo "Install in test frontend for react 16"
-(cd ./examples/for-tests-react-16 && npm i -d --force --legacy-peer-deps && npm run prep) || exit $?
+(cd ./examples/for-tests-react-16 && GIT_SSH_COMMAND="ssh" npm i -d --force --legacy-peer-deps && npm run prep) || exit $?
 
 echo "Install in type check test project"
-(cd ./test/with-typescript && npm i -d --force) || exit $?
+(cd ./test/with-typescript && GIT_SSH_COMMAND="ssh" npm i -d --force) || exit $?
