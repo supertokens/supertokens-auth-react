9cdd1dff03d5b2563a5d402a97b45a7860903691
Chakravarthi Medicharla <medicharlachakravarthi@gmail.com>
2023-09-26 18:53:46 +0530

    feat: add option to use legacy twitter logo

--- lib/build/recipe/thirdparty/providers/twitter.d.ts
+++ lib/build/recipe/thirdparty/providers/twitter.d.ts
@@ -1,10 +1,14 @@
 /// <reference types="react" />
 import type { BuiltInProviderConfig } from "./types";
 import Provider from ".";
+export declare type TwitterProviderConfig = BuiltInProviderConfig & {
+    useLegacyTwitterLogo?: boolean;
+};
 export default class Twitter extends Provider {
+    private useLegacyTwitterLogo;
     private static instance?;
-    constructor(config?: BuiltInProviderConfig);
+    constructor(config?: TwitterProviderConfig);
     getLogo: () => JSX.Element;
-    static init(config?: BuiltInProviderConfig): Provider;
+    static init(config?: TwitterProviderConfig): Provider;
     static reset(): void;
 }
