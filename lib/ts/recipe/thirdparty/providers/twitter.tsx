a977b080622fd72c73624deb8f5828180c5c6354
Chakravarthy7102 <medicharlachakravarthi@gmail.com>
2023-09-27 15:02:34 +0530

    incorporate review changes

--- lib/ts/recipe/thirdparty/providers/twitter.tsx
+++ lib/ts/recipe/thirdparty/providers/twitter.tsx
@@ -41,7 +41,7 @@ export default class Twitter extends Provider {
     constructor(config?: TwitterProviderConfig) {
         let id = "x";
         let name = "X";
-        const _useLegacyTwitterLogo = Boolean(config?.useLegacyTwitterLogo);
+        const _useLegacyTwitterLogo = config?.useLegacyTwitterLogo === true;
 
         if (_useLegacyTwitterLogo) {
             id = "twitter";
@@ -74,7 +74,7 @@ export default class Twitter extends Provider {
             );
         }
         return (
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 1200 1227">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 1200 1227">
                 <path
                     d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"
                     fill="#000"
