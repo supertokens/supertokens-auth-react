d1c3a9c81f73ee696d11c7c600e24b89fdc34d8c
Mihaly Lengyel <mihaly@lengyel.tech>
2023-10-09 19:57:15 +0200

    ci: make updateExampleAppDeps support with-localstorage

--- test/updateExampleAppDeps.sh
+++ test/updateExampleAppDeps.sh
@@ -2,11 +2,12 @@
 
 cd $1;
 npm i;
+npm install git+https://github.com:supertokens/supertokens-auth-react.git#$GITHUB_SHA;
 
 if [ -d "frontend" ]; then
     pushd frontend;
     npm i;
-    npm install git+https://github.com:supertokens/supertokens-auth-react.git#$GITHUB_SHA;
+    grep supertokens-auth-react package.json && npm install git+https://github.com:supertokens/supertokens-auth-react.git#$GITHUB_SHA;
     popd;
 else
     npm install git+https://github.com:supertokens/supertokens-auth-react.git#$GITHUB_SHA;
