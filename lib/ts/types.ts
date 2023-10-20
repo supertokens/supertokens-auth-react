e2b95de92dcdb07a88dff965b4a97b45ddaef27d
amitbadala <amitbadala07@gmail.com>
2023-10-20 15:03:53 +0530

    Remove inputComp from NormalizedFormField

--- lib/ts/types.ts
+++ lib/ts/types.ts
@@ -289,11 +289,6 @@ export type NormalisedFormField = {
      * Moves focus to the input element when it mounts
      */
     autofocus?: boolean;
-
-    /*
-     * Ability to add custom components
-     */
-    inputComponent?: React.FC<InputProps>;
 };
 
 export type ReactComponentClass<P = any> = ComponentClass<P, any> | ((props: P) => JSX.Element);
