2480ebde0708950e598bdbed56207de7c700afd0
amitbadala <amitbadala07@gmail.com>
2023-10-26 18:34:09 +0530

    instead of form submit apply type test within onChange function itself

--- lib/ts/recipe/emailpassword/components/library/formBase.tsx
+++ lib/ts/recipe/emailpassword/components/library/formBase.tsx
@@ -120,6 +120,14 @@ export const FormBase: React.FC<FormBaseProps<any>> = (props) => {
 
     const onInputChange = useCallback(
         (field: APIFormField) => {
+            try {
+                if (typeof field.value !== "string") {
+                    throw new Error(`${field.id} value must be a string`);
+                }
+            } catch (error) {
+                console.error(error);
+                return props.onError("SOMETHING_WENT_WRONG_ERROR");
+            }
             updateFieldState(field.id, (os) => ({ ...os, value: field.value, error: undefined }));
             props.clearError();
         },
@@ -144,19 +152,6 @@ export const FormBase: React.FC<FormBaseProps<any>> = (props) => {
                     value: fieldState === undefined ? "" : fieldState.value,
                 };
             });
-
-            // field.value must be a string
-            try {
-                const fieldsWithIncorrectValues = apiFields.filter((field) => typeof field.value !== "string");
-                if (fieldsWithIncorrectValues.length > 0) {
-                    const errorFields = fieldsWithIncorrectValues.map(({ id }) => id).join(", ");
-                    throw new Error(`${errorFields} value must be a string`);
-                }
-            } catch (error) {
-                console.error(error);
-                setIsLoading(false);
-                return props.onError("SOMETHING_WENT_WRONG_ERROR");
-            }
 
             const fieldUpdates: FieldState[] = [];
             // Call API.
