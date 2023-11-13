907930e91173c186878e4c06f690ed07037ad82b
Mihaly Lengyel <mihaly@lengyel.tech>
2023-11-13 09:02:45 +0100

    feat: make sure firstFactor conflicts hit user error boundaries

--- lib/build/recipe/passwordless/utils.d.ts
+++ lib/build/recipe/passwordless/utils.d.ts
@@ -1,2 +1,7 @@
 import type { Config, NormalisedConfig } from "./types";
+import type { DynamicLoginMethodsContextValue } from "../multitenancy/dynamicLoginMethodsContext";
 export declare function normalisePasswordlessConfig(config: Config): NormalisedConfig;
+export declare function getEnabledContactMethods(
+    contactMethod: "PHONE" | "EMAIL" | "EMAIL_OR_PHONE",
+    currentDynamicLoginMethods: DynamicLoginMethodsContextValue
+): string[];
