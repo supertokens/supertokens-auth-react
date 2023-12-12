e39c5ba0e02c97ca18698ce0e248bc51034171de
Mihaly Lengyel <mihaly@lengyel.tech>
2023-12-12 15:24:22 +0100

    fix: add missing type to prebuiltuis

--- lib/ts/ui/types.ts
+++ lib/ts/ui/types.ts
@@ -5,6 +5,7 @@ import type { PasswordlessPreBuiltUI } from "../recipe/passwordless/prebuiltui";
 import type { ThirdPartyPreBuiltUI } from "../recipe/thirdparty/prebuiltui";
 import type { ThirdPartyEmailPasswordPreBuiltUI } from "../recipe/thirdpartyemailpassword/prebuiltui";
 import type { ThirdPartyPasswordlessPreBuiltUI } from "../recipe/thirdpartypasswordless/prebuiltui";
+import type { TOTPPreBuiltUI } from "../recipe/totp/prebuiltui";
 
 export type ReactRouterDomWithCustomHistory = {
     router: { Route: any };
@@ -20,4 +21,6 @@ export type PreBuiltRecipes = (
     | typeof PasswordlessPreBuiltUI
     | typeof EmailVerificationPreBuiltUI
     | typeof MultiFactorAuthPreBuiltUI
+    | typeof MultiFactorAuthPreBuiltUI
+    | typeof TOTPPreBuiltUI
 )[];
