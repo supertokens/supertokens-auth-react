e77430d149baf61ee07e607d8f09f24ad3cfc05d
Mihaly Lengyel <mihaly@lengyel.tech>
2023-12-22 00:12:23 +0100

    docs: update emailverification-with-otp example to use MFA

--- examples/with-emailverification-with-otp/api-server/server.ts
+++ examples/with-emailverification-with-otp/api-server/server.ts
@@ -3,9 +3,10 @@ import supertokens from "supertokens-node";
 import Session from "supertokens-node/recipe/session";
 import ThirdPartyEmailPassword from "supertokens-node/recipe/thirdpartyemailpassword";
 import EmailVerification from "supertokens-node/recipe/emailverification";
+import MultiFactorAuth from "supertokens-node/recipe/multifactorauth";
+import Passwordless from "supertokens-node/recipe/passwordless";
 import { middleware, errorHandler } from "supertokens-node/framework/express";
 import cors from "cors";
-import { generateOtpAndMapToToken, mailTransporter, getMessageBody } from "./utils";
 import Dashboard from "supertokens-node/recipe/dashboard";
 
 import dotenv from "dotenv";
@@ -34,68 +35,19 @@ supertokens.init({
     recipeList: [
         EmailVerification.init({
             mode: "REQUIRED",
-            emailDelivery: {
-                service: {
-                    sendEmail: async ({ user, emailVerifyLink: url }) => {
-                        // retrieve the token from the url
-                        const token = new URL(url).searchParams.get("token");
-
-                        if (token !== null) {
-                            // generate a 6 digit otp
-                            let otp = generateOtpAndMapToToken(token, otpToTokenMapping);
-                            console.log(otp, user.email);
-                            try {
-                                /**
-                                 * This will not work if you have not set up your email credentials in the .env file. Refer to .env.example
-                                 * in this example app to know which environment variables you need to set.
-                                 */
-                                await mailTransporter.sendMail({
-                                    from: process.env.NODEMAILER_USER,
-                                    to: user.email,
-                                    subject: "SuperTokens Demo OTP",
-                                    html: getMessageBody(otp, user.email),
-                                });
-                            } catch {
-                                // We ignore this in the example so the example is still usable by getting the otp from the console
-                            }
-                        }
-                    },
-                },
-            },
+        }),
+        MultiFactorAuth.init({
+            firstFactors: ["thirdparty", "emailpassword"], // This is basically disallows using passwordless to sign in
             override: {
-                apis: (oI) => {
-                    return {
-                        ...oI,
-                        verifyEmailPOST: async (input) => {
-                            if (oI.verifyEmailPOST === undefined) {
-                                throw Error("should not come here");
-                            }
-
-                            // retrieve the otp from input
-                            let otp = input.token;
-
-                            // retrieve the token mapped to the otp if it exists
-                            let superTokensToken = otpToTokenMapping.get(otp);
-                            console.log(superTokensToken);
-                            if (superTokensToken !== undefined) {
-                                // if the mapping exists set the token value in the input object to the retrieved token.
-                                input.token = superTokensToken;
-
-                                // remove the otp and token from the mapping
-                                otpToTokenMapping.delete(otp);
-                            } else {
-                                // If the mapping does not exist return an invalid token error
-                                return {
-                                    status: "EMAIL_VERIFICATION_INVALID_TOKEN_ERROR",
-                                };
-                            }
-
-                            let response = await oI.verifyEmailPOST(input);
-                            return response;
-                        },
-                    };
-                },
+                functions: (oI) => ({
+                    ...oI,
+                    getMFARequirementsForAuth: async () => ["otp-email"],
+                }),
             },
+        }),
+        Passwordless.init({
+            contactMethod: "EMAIL",
+            flowType: "USER_INPUT_CODE",
         }),
         ThirdPartyEmailPassword.init({
             providers: [
