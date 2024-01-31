2c7fdd9a4264c28bb22312926566b8db36dd604c
Ankit Tiwari <ankucodes@gmail.com>
2024-01-31 14:33:26 +0530

    fix: remove !important from css styles (#791)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,6 +11,22 @@ const config: StorybookConfig = {
         "@storybook/addon-designs",
         // "@storybook/addon-actions/register",
     ],
+    async webpackFinal(config, { configType }) {
+        if (config.module?.rules) {
+            // By default, Storybook utilizes the style-loader, injecting CSS directly into the DOM.
+            // In our build process, we import the CSS string and embed it within a style tag.
+            // The follwoing changes ensure that we replicate this behavior in Storybook.
+            config.module.rules = config.module.rules.filter(
+                (rule) => !(typeof rule === "object" && rule?.test instanceof RegExp && rule.test.test(".css"))
+            );
+
+            config.module.rules.push({
+                test: /\.css$/,
+                use: ["to-string-loader", "css-loader"],
+            });
+        }
+        return config;
+    },
     framework: {
         name: "@storybook/react-webpack5",
         options: {},
