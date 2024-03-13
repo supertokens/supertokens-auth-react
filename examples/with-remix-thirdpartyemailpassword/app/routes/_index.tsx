52c9b578f152ee6d323e05075824423c66751d57
Mark Wentowski <mark@supertokens.com>
2024-03-12 20:54:53 -0500

    attempting to use getSession directly as example

--- examples/with-remix-thirdpartyemailpassword/app/routes/_index.tsx
+++ examples/with-remix-thirdpartyemailpassword/app/routes/_index.tsx
@@ -3,6 +3,84 @@ import { useSessionContext } from "supertokens-auth-react/recipe/session/index.j
 import { signOut } from "supertokens-auth-react/recipe/thirdpartyemailpassword/index.js";
 import { recipeDetails } from "../config/frontend";
 import SuperTokens from "supertokens-auth-react";
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { CollectingResponse } from "supertokens-node/lib/build/framework/custom/index.js";
+import Session from "supertokens-node/lib/build/recipe/session/index.js"
+
+export type BaseRequest = {
+  method: string;
+  url: string;
+  query: string;
+  headers: Headers;
+  formData: () => Promise<FormData>;
+  json: () => Promise<unknown>;
+  cookies: {
+    getAll: () => { name: string; value: string }[];
+  };
+}
+
+function createBaseRequest(request: Request): BaseRequest {
+  const headers = new Headers();
+  request.headers.forEach((value, key) => {
+    headers.append(key, value);
+  });
+
+  return {
+    method: request.method as string,
+    url: request.url as string,
+    query: new URL(request.url).searchParams.toString(),
+    headers: headers,
+    formData: async () => await request.formData(),
+    json: async () => await request.json(),
+    cookies: {
+      getAll: () => {
+        const cookieHeader = request.headers.get("Cookie");
+        if (cookieHeader) {
+          return cookieHeader.split(";").map((cookieString) => {
+            const [name, value] = cookieString.trim().split("=");
+            return { name, value } as { name: string; value: string };
+          });
+        } else {
+          return [];
+        }
+      },
+    },
+  };
+}
+
+// In a Remix application, the loader function is responsible for handling server-side logic. When a user accesses a specific route in your application, Remix calls the corresponding loader function to fetch data or perform any necessary server-side operations before rendering the page.
+
+// Check if window is defined to determine if code is running in the browser
+const isBrowser = typeof window !== 'undefined';
+
+// Use isBrowser to conditionally execute client-side code
+export async function loader({ request }: LoaderFunctionArgs) {
+  try {
+    const baseRequest = createBaseRequest(request);
+    console.log("The base request looks like:", baseRequest);
+
+    // Use CollectingResponse only on the server-side
+    console.log("isBrowser is:", isBrowser)
+    const baseResponse = isBrowser ? null : new CollectingResponse();
+    console.log("The baseRespons looks like:", baseResponse);
+
+    const options = undefined;
+    const userContext = null;
+    if (!isBrowser) {
+      console.log("Running on server");
+      const session = await Session.getSession(baseRequest, baseResponse, options, userContext);
+      console.log("The session looks like:", session);
+    } else {
+      console.warn("Session.getSession called in browser environment. This should only be called on the server-side.");
+    }
+    return null;
+  } catch (error) {
+    return json({ error: "Internal server error" }, { status: 500 });
+  }
+}
+
+
+// sAccessToken, sFrontToken, st-last-access-token-update
 
 export default function Home() {
   const session = useSessionContext();
