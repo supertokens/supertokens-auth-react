6b11c98ab31b383908d5965f967d1fd64f7f09b3
rishabhpoddar <rishabh.poddar@gmail.com>
2024-07-16 12:43:51 +0530

    adding dev-v0.38.2 tag to this commit to ensure building

--- lib/build/recipe/thirdparty/components/themes/signInAndUpCallback/index.d.ts
+++ lib/build/recipe/thirdparty/components/themes/signInAndUpCallback/index.d.ts
@@ -1,3 +1,4 @@
-/// <reference types="react" />
 import type { NormalisedConfig } from "../../../types";
-export declare const SignInAndUpCallbackTheme: (props: { config: NormalisedConfig }) => JSX.Element;
+export declare const SignInAndUpCallbackTheme: (props: {
+    config: NormalisedConfig;
+}) => import("react/jsx-runtime").JSX.Element;
