de90d562083f155afe99aff6832ea8b532eea0ac
Mihály Lengyel <mihaly@lengyel.tech>
2024-08-12 01:11:54 +0200

    Revert "fix: improve error messages when the authpage has issues covering all…"
    This reverts commit b6f4657ddffe19b6a479543b386cc1d1538bf0f4.

--- lib/ts/recipe/authRecipe/components/feature/authPage/authPage.tsx
+++ lib/ts/recipe/authRecipe/components/feature/authPage/authPage.tsx
@@ -354,10 +354,6 @@ async function buildAndSetChildProps(
         }
     }
 
-    if (firstFactors.length === 0) {
-        throw new Error("There are no enabled factors to show");
-    }
-
     if (firstFactors.includes(FactorIds.THIRDPARTY)) {
         // we get the thirdparty recipe here like this, because importing the recipe here would heavily increase the bundle size of many recipes
         const thirdPartyPreBuiltUI = recipeRouters.find((r) => r.recipeInstance.recipeID === FactorIds.THIRDPARTY);
@@ -372,11 +368,6 @@ async function buildAndSetChildProps(
                 (!SuperTokens.usesDynamicLoginMethods || loadedDynamicLoginMethods!.thirdparty.providers.length === 0)
             ) {
                 firstFactors = firstFactors.filter((f) => f !== FactorIds.THIRDPARTY);
-                if (firstFactors.length === 0) {
-                    throw new Error(
-                        "The only enabled first factor is thirdparty, but no providers were defined. Please define at least one provider."
-                    );
-                }
             }
         }
     }
@@ -444,26 +435,7 @@ async function buildAndSetChildProps(
     const selectedComponents = selectComponentsToCoverAllFirstFactors(partialAuthComps, firstFactors);
 
     if (selectedComponents === undefined) {
-        const availableFactors = new Set();
-        for (const comp of partialAuthComps) {
-            for (const id of comp.factorIds) {
-                availableFactors.add(id);
-            }
-        }
-        const source =
-            factorListState !== undefined
-                ? "local state or props"
-                : loadedDynamicLoginMethods?.firstFactors !== undefined
-                ? "dynamic tenant configuration"
-                : MultiFactorAuth.getInstance()?.config.firstFactors !== undefined
-                ? "the config passed to the MFA recipe"
-                : "all recipes initialized";
-
-        throw new Error(
-            `Couldn't cover all first factors: ${firstFactors.join(
-                ", "
-            )} (from ${source}), available components: ${Array.from(availableFactors).join(", ")}`
-        );
+        throw new Error("Couldn't cover all first factors");
     }
 
     setComponentListInfo({
