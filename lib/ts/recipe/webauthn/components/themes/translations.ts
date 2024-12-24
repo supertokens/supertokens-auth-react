115531887aac24532634c6148ed470dd16670592
Deepjyoti Barman <deepjyoti@supertokens.com>
2024-12-24 15:10:12 +0530

    Update the continue with passwordless button text

--- lib/ts/recipe/webauthn/components/themes/translations.ts
+++ lib/ts/recipe/webauthn/components/themes/translations.ts
@@ -7,6 +7,6 @@ export const defaultTranslationsWebauthn = {
         // WEBAUTHN_CONTINUE_WITHOUT_PASSKEY_BUTTON: "Continue without passkey",
         // WEBAUTHN_CREATE_A_PASSKEY_HEADER: "Create a passkey",
         // WEBAUTHN_CONTINUE_WITH_EMAIL_SUBTEXT: "Continue with",
-        WEBAUTHN_COMBO_CONTINUE_WITH_PASSKEY_BUTTON: "Continue with passkey",
+        WEBAUTHN_COMBO_CONTINUE_WITH_PASSKEY_BUTTON: "CONTINUE WITH PASSKEY",
     },
 };
