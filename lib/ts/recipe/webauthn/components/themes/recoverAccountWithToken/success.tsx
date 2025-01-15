ae49de015c73966a6e2dcef885de29c7f09de15a
Deepjyoti Barman <deepjyoti@supertokens.com>
2025-01-15 12:02:19 +0530

    Move more svg components to assets

--- lib/ts/recipe/webauthn/components/themes/recoverAccountWithToken/success.tsx
+++ lib/ts/recipe/webauthn/components/themes/recoverAccountWithToken/success.tsx
@@ -14,6 +14,7 @@
  */
 
 import { redirectToAuth } from "../../../../..";
+import RecoverySuccessIcon from "../../../../../components/assets/recoverySuccessIcon";
 import { useTranslation } from "../../../../../translation/translationContext";
 import { Button } from "../../../../emailpassword/components/library";
 
@@ -27,14 +28,7 @@ export const PasskeyRecoverAccountSuccess = (): JSX.Element => {
     return (
         <div data-supertokens="passkeyRecoverAccountSuccessContainer">
             <div data-supertokens="header">
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M7.08 9.72L5.4 11.4L10.8 16.8L22.8 4.8L21.12 3.12L10.8 13.44L7.08 9.72ZM21.6 12C21.6 17.28 17.28 21.6 12 21.6C6.72 21.6 2.4 17.28 2.4 12C2.4 6.72 6.72 2.4 12 2.4C12.96 2.4 13.8 2.52 14.64 2.76L16.56 0.84C15.12 0.36 13.56 0 12 0C5.4 0 0 5.4 0 12C0 18.6 5.4 24 12 24C18.6 24 24 18.6 24 12H21.6Z"
-                        fill="#40A700"
-                    />
-                </svg>
+                <RecoverySuccessIcon />
                 <div data-supertokens="headerText">{t("WEBAUTHN_ACCOUNT_RECOVERY_SUCCESSFUL_LABEL")}</div>
             </div>
             <div data-supertokens="divider"></div>
