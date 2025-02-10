ef0c66bebc5b141ec12d1d643fb6018840c4ae02
Deepjyoti Barman <deepjyoti@supertokens.com>
2025-02-10 15:57:03 +0530

    Add test to test recovery of account using token

--- test/end-to-end/webauthn.recover_account.test.js
+++ test/end-to-end/webauthn.recover_account.test.js
@@ -7,6 +7,7 @@ import {
     clearBrowserCookiesWithoutAffectingConsole,
     getTestEmail,
     waitForSTElement,
+    submitForm,
 } from "../helpers";
 import { openRecoveryWithToken, signUpAndSendRecoveryEmail, getTokenFromEmail } from "./webauthn.helpers";
 import assert from "assert";
@@ -102,6 +103,65 @@ describe("SuperTokens Webauthn Recover Account", () => {
                 "ST_LOGS WEBAUTHN OVERRIDE GET REGISTER OPTIONS",
                 "ST_LOGS WEBAUTHN PRE_API_HOOKS REGISTER_OPTIONS",
                 "ST_LOGS WEBAUTHN PRE_API_HOOKS REGISTER_OPTIONS",
+            ]);
+        });
+        it("should recover the account successfully", async () => {
+            // Get the token from the email
+            const token = await getTokenFromEmail(email);
+            assert.ok(token);
+            assert.strictEqual(token.length, 128);
+
+            // Use the token to recover the account
+            await openRecoveryWithToken(page, token);
+
+            // We should be in the recovery page now, click the continue button
+            await submitForm(page);
+
+            await new Promise((res) => setTimeout(res, 2000));
+
+            const successContainer = await waitForSTElement(page, "[data-supertokens~='headerText']");
+            const headerText = await successContainer.evaluate((el) => el.textContent);
+
+            // Assert the text contains "Account recovered successfully!"
+            assert.deepStrictEqual(headerText, "Account recovered successfully!");
+
+            // Click on the continue button
+            await submitForm(page);
+
+            await new Promise((res) => setTimeout(res, 2000));
+
+            assert.deepStrictEqual(consoleLogs, [
+                "ST_LOGS SESSION OVERRIDE ADD_FETCH_INTERCEPTORS_AND_RETURN_MODIFIED_FETCH",
+                "ST_LOGS SESSION OVERRIDE ADD_AXIOS_INTERCEPTORS",
+                "ST_LOGS WEBAUTHN OVERRIDE GET REGISTER OPTIONS WITH SIGN UP",
+                "ST_LOGS WEBAUTHN OVERRIDE GET REGISTER OPTIONS",
+                "ST_LOGS WEBAUTHN PRE_API_HOOKS REGISTER_OPTIONS",
+                "ST_LOGS WEBAUTHN OVERRIDE REGISTER CREDENTIAL",
+                "ST_LOGS WEBAUTHN OVERRIDE SIGN UP",
+                "ST_LOGS WEBAUTHN PRE_API_HOOKS SIGN_UP",
+                "ST_LOGS SESSION ON_HANDLE_EVENT SESSION_CREATED",
+                "ST_LOGS SESSION OVERRIDE GET_USER_ID",
+                "ST_LOGS SUPERTOKENS GET_REDIRECTION_URL SUCCESS undefined",
+                "ST_LOGS SESSION OVERRIDE GET_USER_ID",
+                "ST_LOGS SESSION OVERRIDE SIGN_OUT",
+                "ST_LOGS SESSION PRE_API_HOOKS SIGN_OUT",
+                "ST_LOGS SESSION ON_HANDLE_EVENT SIGN_OUT",
+                "ST_LOGS SESSION OVERRIDE ADD_FETCH_INTERCEPTORS_AND_RETURN_MODIFIED_FETCH",
+                "ST_LOGS SESSION OVERRIDE ADD_AXIOS_INTERCEPTORS",
+                "ST_LOGS WEBAUTHN GET_REDIRECTION_URL SEND_RECOVERY_EMAIL",
+                "ST_LOGS WEBAUTHN OVERRIDE GENERATE RECOVER ACCOUNT TOKEN",
+                "ST_LOGS WEBAUTHN PRE_API_HOOKS GENERATE_RECOVER_ACCOUNT_TOKEN",
+                "ST_LOGS SESSION OVERRIDE ADD_FETCH_INTERCEPTORS_AND_RETURN_MODIFIED_FETCH",
+                "ST_LOGS SESSION OVERRIDE ADD_AXIOS_INTERCEPTORS",
+                "ST_LOGS WEBAUTHN OVERRIDE GET REGISTER OPTIONS",
+                "ST_LOGS WEBAUTHN OVERRIDE GET REGISTER OPTIONS",
+                "ST_LOGS WEBAUTHN PRE_API_HOOKS REGISTER_OPTIONS",
+                "ST_LOGS WEBAUTHN PRE_API_HOOKS REGISTER_OPTIONS",
+                "ST_LOGS WEBAUTHN OVERRIDE REGISTER CREDENTIAL",
+                "ST_LOGS WEBAUTHN PRE_API_HOOKS RECOVER_ACCOUNT",
+                "ST_LOGS SUPERTOKENS GET_REDIRECTION_URL TO_AUTH",
+                "ST_LOGS SESSION OVERRIDE ADD_FETCH_INTERCEPTORS_AND_RETURN_MODIFIED_FETCH",
+                "ST_LOGS SESSION OVERRIDE ADD_AXIOS_INTERCEPTORS",
             ]);
         });
     });
