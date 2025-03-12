07aa01fdfc4b6ae87e360928236c81f61ac7c4f3
Namit Nathwani <namsnath@gmail.com>
2025-03-12 16:51:56 +0530

    removes usages of old helper functions

--- test/end-to-end/thirdpartypasswordless.test.js
+++ test/end-to-end/thirdpartypasswordless.test.js
@@ -30,7 +30,6 @@ import {
     getPasswordlessDevice,
     getFeatureFlags,
     assertProviders,
-    setEnabledRecipes,
     clickOnProviderButtonWithoutWaiting,
     getGeneralError,
     waitForUrl,
@@ -227,7 +226,6 @@ describe("SuperTokens Third Party Passwordless", function () {
                 enabledRecipes: ["thirdparty"],
                 enabledProviders: [],
             });
-            await setEnabledRecipes(["thirdparty"], []);
 
             await Promise.all([
                 page.waitForResponse(
