e57d8fef40d8fec6a84b5c15bf1979517069d86b
Namit Nathwani <namsnath@gmail.com>
2025-03-14 13:47:37 +0530

    fix: broken tests, update configs

---
+++ test/end-to-end/mfa.requirement_handling.test.js
@@ -0,0 +1,243 @@
+/* Copyright (c) 2021, VRAI Labs and/or its affiliates. All rights reserved.
+ *
+ * This software is licensed under the Apache License, Version 2.0 (the
+ * "License") as published by the Apache Software Foundation.
+ *
+ * You may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*
+ * Imports
+ */
+
+import assert from "assert";
+import {
+    clearBrowserCookiesWithoutAffectingConsole,
+    screenshotOnFailure,
+    getTestEmail,
+    getFactorChooserOptions,
+    isMFASupported,
+    setupBrowser,
+    backendHook,
+    setupCoreApp,
+    setupST,
+} from "../helpers";
+import { TEST_CLIENT_BASE_URL } from "../constants";
+import { getTestPhoneNumber } from "../exampleTestHelpers";
+import {
+    tryEmailPasswordSignUp,
+    waitForDashboard,
+    completeOTP,
+    setupOTP,
+    tryEmailPasswordSignIn,
+    chooseFactor,
+    setupTOTP,
+    completeTOTP,
+    goToFactorChooser,
+} from "./mfa.helpers";
+
+/*
+ * Tests.
+ */
+describe("SuperTokens SignIn w/ MFA", function () {
+    let browser;
+    let page;
+    let consoleLogs = [];
+
+    const appConfig = {
+        accountLinkingConfig: {
+            enabled: true,
+            shouldAutoLink: {
+                shouldAutomaticallyLink: true,
+                shouldRequireVerification: false,
+            },
+        },
+    };
+
+    before(async function () {
+        if (!(await isMFASupported())) {
+            this.skip();
+        }
+        await backendHook("before");
+        browser = await setupBrowser();
+
+        const coreUrl = await setupCoreApp();
+        appConfig.coreUrl = coreUrl;
+        await setupST(appConfig);
+    });
+
+    beforeEach(async function () {
+        await backendHook("beforeEach");
+        page = await browser.newPage();
+        page.on("console", (consoleObj) => {
+            const log = consoleObj.text();
+            // console.log(log);
+            if (log.startsWith("ST_LOGS")) {
+                consoleLogs.push(log);
+            }
+        });
+        consoleLogs = await clearBrowserCookiesWithoutAffectingConsole(page, []);
+
+        await page.evaluate(() => window.localStorage.removeItem("supertokens-passwordless-loginAttemptInfo"));
+        await page.evaluate(() => window.localStorage.removeItem("clientRecipeListForDynamicLogin"));
+        await page.evaluate(() => window.localStorage.removeItem("mode"));
+        await page.evaluate(() => window.localStorage.setItem("enableAllRecipes", "true"));
+    });
+
+    afterEach(async function () {
+        await screenshotOnFailure(this, browser);
+        await page?.close();
+        await backendHook("afterEach");
+    });
+
+    after(async function () {
+        await browser?.close();
+        await backendHook("after");
+    });
+
+    describe("requirement handling", () => {
+        let email, phoneNumber;
+        let secret;
+        before(async () => {
+            page = await browser.newPage();
+
+            email = await getTestEmail();
+            phoneNumber = getTestPhoneNumber();
+
+            await Promise.all([
+                page.goto(`${TEST_CLIENT_BASE_URL}/auth/?rid=emailpassword`),
+                page.waitForNavigation({ waitUntil: "networkidle0" }),
+            ]);
+
+            consoleLogs = await clearBrowserCookiesWithoutAffectingConsole(page, []);
+
+            await page.evaluate(() => window.localStorage.removeItem("supertokens-passwordless-loginAttemptInfo"));
+            await page.evaluate(() => window.localStorage.removeItem("clientRecipeListForDynamicLogin"));
+            await page.evaluate(() => window.localStorage.setItem("enableAllRecipes", "true"));
+
+            await tryEmailPasswordSignUp(page, email);
+            await waitForDashboard(page);
+            await goToFactorChooser(page);
+            await chooseFactor(page, "otp-email");
+            await completeOTP(page);
+            await setupOTP(page, "PHONE", phoneNumber);
+            secret = await setupTOTP(page);
+
+            await page.close();
+        });
+
+        describe("multistep requirement list", () => {
+            it("multistep requirements should happen in order (allOfInAnyOrder -> oneOf)", async () => {
+                appConfig.mfaInfo = {
+                    requirements: [{ allOfInAnyOrder: ["otp-phone", "totp"] }, { oneOf: ["otp-email"] }],
+                };
+                await setupST(appConfig);
+
+                await tryEmailPasswordSignIn(page, email);
+                const factors1 = await getFactorChooserOptions(page);
+                assert.deepStrictEqual(new Set(factors1), new Set(["otp-phone", "totp"]));
+                await chooseFactor(page, "otp-phone");
+                await completeOTP(page);
+                await completeTOTP(page, secret);
+                await completeOTP(page);
+                await waitForDashboard(page);
+            });
+
+            it("multistep requirements should happen in order (oneOf -> allOfInAnyOrder)", async () => {
+                appConfig.mfaInfo = {
+                    requirements: [{ oneOf: ["otp-phone", "totp"] }, { allOfInAnyOrder: ["totp", "otp-email"] }],
+                };
+                await setupST(appConfig);
+
+                await tryEmailPasswordSignIn(page, email);
+                const factors1 = await getFactorChooserOptions(page);
+                assert.deepStrictEqual(new Set(factors1), new Set(["otp-phone", "totp"]));
+                await chooseFactor(page, "otp-phone");
+                await completeOTP(page);
+                const factors2 = await getFactorChooserOptions(page);
+                assert.deepStrictEqual(new Set(factors2), new Set(["otp-email", "totp"]));
+                await chooseFactor(page, "totp");
+                await completeTOTP(page, secret);
+                await completeOTP(page);
+                await waitForDashboard(page);
+            });
+            it("string requirements strictly set the order of the factor screens", async () => {
+                appConfig.mfaInfo = {
+                    requirements: ["otp-phone", "totp", "otp-email"],
+                };
+                await setupST(appConfig);
+
+                await tryEmailPasswordSignIn(page, email);
+                await completeOTP(page, "PHONE");
+                await completeTOTP(page, secret);
+                await completeOTP(page, "EMAIL");
+                await waitForDashboard(page);
+            });
+        });
+
+        describe("allOfInAnyOrder", () => {
+            it("should pass if all requirements are complete", async () => {
+                appConfig.mfaInfo = {
+                    requirements: [{ allOfInAnyOrder: ["otp-phone", "totp", "otp-email"] }],
+                };
+                await setupST(appConfig);
+
+                await tryEmailPasswordSignIn(page, email);
+                const factors1 = await getFactorChooserOptions(page);
+                assert.deepStrictEqual(new Set(factors1), new Set(["otp-phone", "totp", "otp-email"]));
+                await chooseFactor(page, "otp-phone");
+                await completeOTP(page);
+
+                const factors2 = await getFactorChooserOptions(page);
+                assert.deepStrictEqual(new Set(factors2), new Set(["totp", "otp-email"]));
+                await chooseFactor(page, "otp-email");
+                await completeOTP(page);
+
+                await completeTOTP(page, secret);
+                await waitForDashboard(page);
+            });
+            it("should pass if the array is empty", async () => {
+                appConfig.mfaInfo = {
+                    requirements: [{ allOfInAnyOrder: [] }],
+                };
+                await setupST(appConfig);
+
+                await tryEmailPasswordSignIn(page, email);
+                await waitForDashboard(page);
+            });
+        });
+
+        describe("oneOf", () => {
+            it("should pass if one of the requirements are complete", async () => {
+                appConfig.mfaInfo = {
+                    requirements: [{ oneOf: ["otp-phone", "totp", "otp-email"] }],
+                };
+                await setupST(appConfig);
+
+                await tryEmailPasswordSignIn(page, email);
+                const factors1 = await getFactorChooserOptions(page);
+                assert.deepStrictEqual(new Set(factors1), new Set(["otp-phone", "totp", "otp-email"]));
+                await chooseFactor(page, "otp-phone");
+                await completeOTP(page);
+
+                await waitForDashboard(page);
+            });
+            it("should pass if the array is empty", async () => {
+                appConfig.mfaInfo = {
+                    requirements: [{ oneOf: [] }],
+                };
+                await setupST(appConfig);
+
+                await tryEmailPasswordSignIn(page, email);
+                await waitForDashboard(page);
+            });
+        });
+    });
+});
--- test/end-to-end/passwordless.test.js
+++ test/end-to-end/passwordless.test.js
@@ -18,8 +18,6 @@
  */
 
 import assert from "assert";
-import puppeteer from "puppeteer";
-import fetch from "isomorphic-fetch";
 import {
     clearBrowserCookiesWithoutAffectingConsole,
     getPasswordlessDevice,
@@ -27,24 +25,22 @@ import {
     waitForSTElement,
     waitFor,
     getFeatureFlags,
-    waitForText,
     screenshotOnFailure,
-    setPasswordlessFlowType,
-    isReact16,
-    clickOnPasswordlessResendButton,
     isGeneralErrorSupported,
     setGeneralErrorToLocalStorage,
     getInputField,
     isAccountLinkingSupported,
-    backendBeforeEach,
+    waitForUrl,
+    setupBrowser,
+    clickForgotPasswordLink,
+    backendHook,
+    setupCoreApp,
+    setupST,
 } from "../helpers";
 
-import {
-    TEST_CLIENT_BASE_URL,
-    TEST_SERVER_BASE_URL,
-    TEST_APPLICATION_SERVER_BASE_URL,
-    SOMETHING_WENT_WRONG_ERROR,
-} from "../constants";
+import { TEST_CLIENT_BASE_URL, SOMETHING_WENT_WRONG_ERROR } from "../constants";
+import { tryEmailPasswordSignUp, tryPasswordlessSignInUp } from "./mfa.helpers";
+import { randomUUID } from "crypto";
 
 /*
  * Tests.
@@ -61,9 +57,11 @@ export function getPasswordlessTestCases({ authRecipe, logId, generalErrorRecipe
     let browser;
     let page;
     let consoleLogs = [];
-    const signInUpPageLoadLogs = isReact16()
-        ? [`ST_LOGS ${logId} OVERRIDE GET_LOGIN_ATTEMPT_INFO`]
-        : [`ST_LOGS ${logId} OVERRIDE GET_LOGIN_ATTEMPT_INFO`, `ST_LOGS ${logId} OVERRIDE GET_LOGIN_ATTEMPT_INFO`];
+
+    const signInUpPageLoadLogs = [
+        `ST_LOGS ${logId} OVERRIDE GET_LOGIN_ATTEMPT_INFO`,
+        `ST_LOGS ${logId} OVERRIDE GET_LOGIN_ATTEMPT_INFO`,
+    ];
 
     const examplePhoneNumber = "+36701231212";
     const exampleEmail = "test@example.com";
@@ -349,19 +347,29 @@ export function getPasswordlessTestCases({ authRecipe, logId, generalErrorRecipe
                 const contactMethod = "EMAIL_OR_PHONE";
 
                 before(async function () {
-                    ({ browser, page } = await initBrowser(contactMethod, consoleLogs, authRecipe));
-                    await setPasswordlessFlowType(contactMethod, "USER_INPUT_CODE");
+                    const appId = randomUUID();
+                    const coreUrl = await setupCoreApp({
+                        appId,
+                        coreConfig: {
+                            passwordless_code_lifetime: 4000,
+                            passwordless_max_code_input_attempts: 3,
+                        },
+                    });
+                    await setupST({
+                        coreUrl,
+                        passwordlessFlowType: "USER_INPUT_CODE",
+                        passwordlessContactMethod: contactMethod,
+                    });
+
+                    ({ browser, page } = await initBrowser(contactMethod, consoleLogs, authRecipe, undefined));
+                    if (authRecipe === "all") {
+                        await tryEmailPasswordSignUp(page, registeredEmailWithPass);
+                    }
                 });
 
                 after(async function () {
-                    await browser.close();
-                    await fetch(`${TEST_SERVER_BASE_URL}/after`, {
-                        method: "POST",
-                    }).catch(console.error);
-
-                    await fetch(`${TEST_SERVER_BASE_URL}/stopst`, {
-                        method: "POST",
-                    }).catch(console.error);
+                    await browser?.close();
+                    await backendHook("after");
                 });
 
                 beforeEach(async function () {
@@ -533,23 +541,43 @@ export function getPasswordlessTestCases({ authRecipe, logId, generalErrorRecipe
     });
 
     function getTestCases(contactMethod, inputName, contactInfo) {
+        let contactInfoSubmitLogs =
+            authRecipe === "all" && inputName == "email"
+                ? contactInfoSubmitLogsWithEmailChecks
+                : contactInfoSubmitLogsWithoutEmailChecks;
+
         let accountLinkingSupported;
+        let coreUrl;
+        const appId = randomUUID();
+        const coreConfig = {
+            passwordless_code_lifetime: 4000,
+            passwordless_max_code_input_attempts: 3,
+        };
+
+        before(async function () {
+            coreUrl = await setupCoreApp({
+                appId,
+                coreConfig,
+            });
+        });
+
         describe(`UserInputCode`, function () {
+            this.timeout(60000);
+
             before(async function () {
-                ({ browser, page } = await initBrowser(contactMethod, consoleLogs, authRecipe));
-                await setPasswordlessFlowType(contactMethod, "USER_INPUT_CODE");
+                await backendHook("before");
+                await setupST({
+                    coreUrl,
+                    passwordlessFlowType: "USER_INPUT_CODE",
+                    passwordlessContactMethod: contactMethod,
+                });
+                ({ browser, page } = await initBrowser(contactMethod, consoleLogs, authRecipe, undefined));
                 accountLinkingSupported = await isAccountLinkingSupported();
             });
 
             after(async function () {
-                await browser.close();
-                await fetch(`${TEST_SERVER_BASE_URL}/after`, {
-                    method: "POST",
-                }).catch(console.error);
-
-                await fetch(`${TEST_SERVER_BASE_URL}/stopst`, {
-                    method: "POST",
-                }).catch(console.error);
+                await browser?.close();
+                await backendHook("after");
             });
 
             beforeEach(async function () {
@@ -1113,20 +1141,19 @@ export function getPasswordlessTestCases({ authRecipe, logId, generalErrorRecipe
 
         describe(`Link`, function () {
             before(async function () {
-                ({ browser, page } = await initBrowser(contactMethod, consoleLogs, authRecipe));
-                await setPasswordlessFlowType(contactMethod, "MAGIC_LINK");
+                await backendHook("before");
+                ({ browser, page } = await initBrowser(contactMethod, consoleLogs, authRecipe, undefined));
                 accountLinkingSupported = await isAccountLinkingSupported();
+                await setupST({
+                    coreUrl,
+                    passwordlessFlowType: "MAGIC_LINK",
+                    passwordlessContactMethod: contactMethod,
+                });
             });
 
             after(async function () {
-                await browser.close();
-                await fetch(`${TEST_SERVER_BASE_URL}/after`, {
-                    method: "POST",
-                }).catch(console.error);
-
-                await fetch(`${TEST_SERVER_BASE_URL}/stopst`, {
-                    method: "POST",
-                }).catch(console.error);
+                await browser?.close();
+                await backendHook("after");
             });
 
             beforeEach(async function () {
@@ -1644,20 +1671,19 @@ export function getPasswordlessTestCases({ authRecipe, logId, generalErrorRecipe
 
         describe(`Link/Code`, function () {
             before(async function () {
-                ({ browser, page } = await initBrowser(contactMethod, consoleLogs, authRecipe));
-                await setPasswordlessFlowType(contactMethod, "USER_INPUT_CODE_AND_MAGIC_LINK");
+                await backendHook("before");
+                await setupST({
+                    coreUrl,
+                    passwordlessFlowType: "USER_INPUT_CODE_AND_MAGIC_LINK",
+                    passwordlessContactMethod: contactMethod,
+                });
+                ({ browser, page } = await initBrowser(contactMethod, consoleLogs, authRecipe, undefined));
                 accountLinkingSupported = await isAccountLinkingSupported();
             });
 
             after(async function () {
-                await browser.close();
-                await fetch(`${TEST_SERVER_BASE_URL}/after`, {
-                    method: "POST",
-                }).catch(console.error);
-
-                await fetch(`${TEST_SERVER_BASE_URL}/stopst`, {
-                    method: "POST",
-                }).catch(console.error);
+                await browser?.close();
+                await backendHook("after");
             });
 
             beforeEach(async function () {
@@ -2179,6 +2205,56 @@ export function getPasswordlessTestCases({ authRecipe, logId, generalErrorRecipe
                 await waitForSTElement(page, "[data-supertokens~=input][name=userInputCode]");
             });
         });
+
+        if (authRecipe === "all") {
+            describe("with emailpassword combo", () => {
+                before(async function () {
+                    await backendHook("before");
+                    await setupST({
+                        coreUrl,
+                        passwordlessFlowType: "USER_INPUT_CODE",
+                        passwordlessContactMethod: contactMethod,
+                    });
+                    ({ browser, page } = await initBrowser(contactMethod, consoleLogs, authRecipe, undefined));
+                    if (authRecipe === "all") {
+                        await tryEmailPasswordSignUp(page, registeredEmailWithPass);
+                    }
+                });
+
+                after(async function () {
+                    await browser?.close();
+                    await backendHook("after");
+                });
+
+                beforeEach(async function () {
+                    await clearBrowserCookiesWithoutAffectingConsole(page, consoleLogs);
+
+                    await page.evaluate(() => localStorage.removeItem("supertokens-passwordless-loginAttemptInfo"));
+                    await page.evaluate(() => localStorage.removeItem("SHOW_GENERAL_ERROR"));
+
+                    consoleLogs.length = 0;
+                });
+
+                it("should navigate to the sign in page when the user clicks on the forgot password link", async function () {
+                    await page.goto(`${TEST_CLIENT_BASE_URL}/auth`);
+
+                    await setInputValues(page, [{ name: "email", value: registeredEmailWithPass }]);
+                    await submitForm(page);
+
+                    const testVal = "nav check" + Date.now();
+
+                    await page.evaluate((testVal) => {
+                        window.testVal = testVal;
+                    }, testVal);
+
+                    await clickForgotPasswordLink(page);
+                    await waitForUrl(page, "/auth/reset-password");
+
+                    const testValAfterNav = await page.evaluate(() => window.testVal);
+                    assert.strictEqual(testVal, testValAfterNav);
+                });
+            });
+        }
     }
 }
 
@@ -2218,24 +2294,8 @@ async function setupDevice(page, inputName, contactInfo, forLinkOnly = true, cle
     return getPasswordlessDevice(loginAttemptInfo);
 }
 
-async function initBrowser(contactMethod, consoleLogs, authRecipe, { defaultCountry, disablePhoneGuess } = {}) {
-    await backendBeforeEach();
-
-    await fetch(`${TEST_SERVER_BASE_URL}/startst`, {
-        method: "POST",
-        headers: [["content-type", "application/json"]],
-        body: JSON.stringify({
-            coreConfig: {
-                passwordless_code_lifetime: 4000,
-                passwordless_max_code_input_attempts: 3,
-            },
-        }),
-    }).catch(console.error);
-
-    const browser = await puppeteer.launch({
-        args: ["--no-sandbox", "--disable-setuid-sandbox", "--remote-debugging-port=9222"],
-        headless: true,
-    });
+async function initBrowser(contactMethod, consoleLogs, authRecipe, { defaultCountry } = {}) {
+    const browser = await setupBrowser();
     const page = await browser.newPage();
     page.on("console", (consoleObj) => {
         const log = consoleObj.text();
@@ -2253,6 +2313,18 @@ async function initBrowser(contactMethod, consoleLogs, authRecipe, { defaultCoun
         ),
         page.waitForNavigation({ waitUntil: "networkidle0" }),
     ]);
+
+    await new Promise((res) => setTimeout(res, 500));
+
+    if (["EMAIL", "EMAIL_OR_PHONE"].includes(contactMethod)) {
+        await tryPasswordlessSignInUp(page, exampleEmail, undefined, false);
+        await clearBrowserCookiesWithoutAffectingConsole(page, []);
+    }
+
+    if (["PHONE", "EMAIL_OR_PHONE"].includes(contactMethod)) {
+        await tryPasswordlessSignInUp(page, examplePhoneNumber, undefined, true);
+        await clearBrowserCookiesWithoutAffectingConsole(page, []);
+    }
 
     return { browser, page };
 }
